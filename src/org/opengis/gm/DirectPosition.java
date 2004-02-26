67ff05e8e71da38eeb5d8e77fef99d770973438d
Martin Desruisseaux <desruisseaux@users.noreply.github.com>
2004-02-26 19:35:59 +0000

    Added mutators

--- src/org/opengis/gm/DirectPosition.java
+++ src/org/opengis/gm/DirectPosition.java
@@ -5,6 +5,7 @@ package org.opengis.gm;
 
 // OpenGIS direct dependencies
 import org.opengis.crs.crs.CRS;
+import org.opengis.util.Cloneable;
 
 
 /**
@@ -21,12 +22,13 @@ import org.opengis.crs.crs.CRS;
  * @author <A HREF="http://www.opengis.org">OpenGIS&reg; consortium</A>
  * @version 2.0
  */
-public interface DirectPosition {
+public interface DirectPosition extends Cloneable {
     /**
      * The length of coordinate sequence (the number of entries).
-     * This is determined by the reference system.
+     * This may be less than or equal to the dimensionality of the 
+     * {@linkplain #getCoordinateReferenceSystem() coordinate reference system}.
      *
-     * @return The dimension.
+     * @return The dimensionality of this position.
      * @UML mandatory dimension
      */
     public int getDimension();
@@ -37,17 +39,30 @@ public interface DirectPosition {
      *
      * @return The coordinates
      * @UML mandatory coordinates
+     *
+     * @revisit Should we said that this array is a clone (i.e. change to this array will
+     *          not affect the underlying coordinates). Or should we provides an optionnaly
+     *          pre-allocated array argument?
      */
     public double[] getCoordinates();
 
     /**
-     * Returns the coordinate at the specified dimension.
+     * Returns the ordinate at the specified dimension.
      *
-     * @param  dim The dimension in the range 0 to {@link #getDimension dimension}-1.
+     * @param  dimension The dimension in the range 0 to {@linkplain #getDimension dimension}-1.
      * @return The coordinate at the specified dimension.
-     * @throws IndexOutOfBoundsException if the specified index is out of bounds.
+     * @throws IndexOutOfBoundsException if the specified dimension is out of bounds.
      */
-    public double getCoordinate(int dim) throws IndexOutOfBoundsException;
+    public double getOrdinate(int dimension) throws IndexOutOfBoundsException;
+
+    /**
+     * Sets the ordinate value along the specified dimension.
+     *
+     * @param dimension the dimension for the ordinate of interest.
+     * @param value the ordinate value of interest.
+     * @throws IndexOutOfBoundsException if the specified dimension is out of bounds.
+     */
+    public void setOrdinate(int dimension, double value) throws IndexOutOfBoundsException;
 
     /**
      * The coordinate reference system in which the coordinate is given.
@@ -60,4 +75,9 @@ public interface DirectPosition {
      * @UML association coordinateReferenceSystem
      */
     public CRS getCoordinateReferenceSystem();
+
+    /**
+     * Makes an exact copy of this coordinate.
+     */
+    public DirectPosition clone();
 }
