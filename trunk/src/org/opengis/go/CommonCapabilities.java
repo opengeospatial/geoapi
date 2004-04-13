7c354a2d83cfefe8afb1fe0b29d96f06159cdbb5
crossley <crossley@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2004-04-13 22:20:54 +0000

    using new factories

--- trunk/src/org/opengis/go/CommonCapabilities.java
+++ trunk/src/org/opengis/go/CommonCapabilities.java
@@ -15,92 +15,95 @@ import org.opengis.crs.crs.projection.ProjectionKey;
 import org.opengis.go.spatial.PathType;
 
 /**
- * The <code>CommonCapabilities</code> interface provides
- * runtime information about the capabilities of a given GO-1 implementation. Objects
- * implementing this interface are obtained through the
+ * The <code>CommonCapabilities</code> interface provides runtime information
+ * about the capabilities of a given GO-1 implementation. Objects implementing
+ * this interface are obtained through the
  * <code>CommonFactory.getCapabilities()</code> method.
- *
- * @author  Open GIS Consortium, Inc.
+ * 
+ * @author Open GIS Consortium, Inc.
  * @version $Revision$, $Date$
  */
 public interface CommonCapabilities {
 
     /**
-     * Returns an array of <code>Class</code> objects for the <code>Bounds</code>
-     * interfaces that are supported by a given implementation.
-     * These Class objects can be used in a call to
+     * Returns an array of <code>Class</code> objects for the
+     * <code>Bounds</code> interfaces that are supported by a given
+     * implementation. These Class objects can be used in a call to
      * <code>BoundsFactory.getBounds(Class)</code>.
      */
-    public Class[] getSupportedBounds();
+    Class[] getSupportedBounds();
 
     /**
-     * Returns an array of <code>Class</code> objects for the
-     * directposition interfaces that are supported by a given implementation.
+     * Returns an array of <code>Class</code> objects for the directposition
+     * interfaces that are supported by a given implementation.
      */
-    public Class[] getSupportedDirectPositions();
+    Class[] getSupportedDirectPositions();
 
     /**
-     * Returns an array of <code>Class</code> objects for the
-     * directposition interfaces that are supported by a given implementation
-     * for the specified Coordinate Reference System.
+     * Returns an array of <code>Class</code> objects for the directposition
+     * interfaces that are supported by a given implementation for the specified
+     * Coordinate Reference System.
+     * 
      * @param crs the Coordinate Reference System.
      */
-    public Class[] getSupportedDirectPositions(CoordinateReferenceSystem crs);
-
-	/**
-	 * Returns an array of <code>Class</code> objects from the
-	 * spatialschema-package that are supported by a given implementation
-	 * for the specified Coordinate Reference System.
-	 * @param crs the Coordinate Reference System.
-	 */
-	public Class[] getSupportedSpatialSchemaObjects(CoordinateReferenceSystem crs);
+    Class[] getSupportedDirectPositions(CoordinateReferenceSystem crs);
 
     /**
-     * Returns an array of <code>Class</code> objects for the
-     * orientation interfaces that are supported by a given implementation.
-     */
-    public Class[] getSupportedOrientations();
-
-    /**
-     * Returns an array of <code>Class</code> objects for the
-     * orientation interfaces that are supported by a given implementation
-     * for the specified Coordinate Reference System.
+     * Returns an array of <code>Class</code> objects from the
+     * spatialschema-package that are supported by a given implementation for
+     * the specified Coordinate Reference System.
+     * 
      * @param crs the Coordinate Reference System.
      */
-    public Class[] getSupportedOrientations(CoordinateReferenceSystem crs);
+    Class[] getSupportedSpatialSchemaObjects(CoordinateReferenceSystem crs);
+
+    /**
+     * Returns an array of <code>Class</code> objects for the orientation
+     * interfaces that are supported by a given implementation.
+     */
+    Class[] getSupportedOrientations();
+
+    /**
+     * Returns an array of <code>Class</code> objects for the orientation
+     * interfaces that are supported by a given implementation for the specified
+     * Coordinate Reference System.
+     * 
+     * @param crs the Coordinate Reference System.
+     */
+    Class[] getSupportedOrientations(CoordinateReferenceSystem crs);
 
     /**
      * Returns an array of <code>Class</code> objects for the geometry
-     * interfaces that are supported by a given implementation.
-     * These Class objects can be used in a call to
+     * interfaces that are supported by a given implementation. These Class
+     * objects can be used in a call to
      * <code>GeometryFactory.getGeometry(Class)</code>.
      */
-    public Class[] getSupportedGeometries();
+    Class[] getSupportedGeometries();
 
     /**
-     * Returns an array of Strings that are keys for the
-     * Coordinate Reference Systems that this implementation supports.
-     * These strings can be used in a call to
+     * Returns an array of Strings that are keys for the Coordinate Reference
+     * Systems that this implementation supports. These strings can be used in a
+     * call to
      * <code>CoordinateReferenceSystemFactory.getCoordinateReferenceSystem(String)</code>.
      */
-    public String[] getSupportedCoordinateReferenceSystems();
+    String[] getSupportedCoordinateReferenceSystems();
 
     /**
-     * Returns an array of Strings that are keys for the datums that
-     * this implementation supports.  These strings can be used in a
-     * call to <code>DatumFactory.getDatum(String)</code>.
+     * Returns an array of Strings that are keys for the datums that this
+     * implementation supports. These strings can be used in a call to
+     * <code>DatumFactory.getDatum(String)</code>.
      */
-    public String[] getSupportedDatums();
+    String[] getSupportedDatums();
 
     /**
-	 * Returns an array of <code>PathType</code> objects that indicates the
-	 * path types that are supported.
-	 */
-    public PathType[] getSupportedPathTypes();
-
-    /**
-     * Returns an array of <code>ProjectionKey</code> objects that indicates the
-     * projections that are supported.
+     * Returns an array of <code>PathType</code> objects that indicates the
+     * path types that are supported.
      */
-    public ProjectionKey[] getSupportedProjections();
+    PathType[] getSupportedPathTypes();
+
+    /**
+     * Returns an array of <code>ProjectionKey</code> objects that indicates
+     * the projections that are supported.
+     */
+    ProjectionKey[] getSupportedProjections();
 }
