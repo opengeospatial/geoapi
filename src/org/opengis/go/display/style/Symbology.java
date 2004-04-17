e4e651131adf8e046da07aef5a3149d0a1a1d27b
crossley <crossley@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2004-04-17 01:05:55 +0000

    Symbology now a part of GraphicStyle, javadoc will be improved in a later commit

--- src/org/opengis/go/display/style/Symbology.java
+++ src/org/opengis/go/display/style/Symbology.java
@@ -15,26 +15,21 @@ package org.opengis.go.display.style;
  * of properties that can exist for a symbol graphic.  This interface allows
  * you to get or set the values of those properties on the graphic.
  */
-public interface Symbology {
-    /**
-     * Gets the name of this symbology object.
-     * @return the symbology name
-     */
-    public String getName();
+public interface Symbology {    
     
     /**
      * Gets the number of properties that exist for the graphic
      * that uses this symbology.
      * @return the number of properties for the graphic
      */
-    public int getPropertyCount();
+    public int getSymbologyPropertyCount(SymbologyInfo info);
     
     /**
      * Gets the list of all the property names that exist for the graphic
      * that uses this symbology.
      * @return the list of property names
      */
-    public String[] getPropertyNames();
+    public String[] getSymbologyPropertyNames(SymbologyInfo info);
     
     /**
      * Gets the java class type for the property value defined by the property
@@ -42,7 +37,7 @@ public interface Symbology {
      * @param propertyName the name of the property in question
      * @return the java class of the property value
      */
-    public Class getPropertyType(String propertyName);
+    public Class getSymbologyPropertyType(SymbologyInfo info, String propertyName);
     
     /**
      * Gets the property value defined by the given property name
@@ -50,7 +45,7 @@ public interface Symbology {
      * @return the value of the property
      * @throws IllegalArgumentException if the property name isn't a valid one
      */
-    public Object getProperty(String propertyName) throws IllegalArgumentException;
+    public Object getSymbologyProperty(SymbologyInfo info, String propertyName) throws IllegalArgumentException;
     
     /**
      * Sets the value of a property for the given property name
@@ -59,6 +54,22 @@ public interface Symbology {
      * @throws IllegalArgumentException if the property name isn't valid or if
      *         the value is of the wrong java class type
      */
-    public void setProperty(String propertyName, Object value)
+    public void setSymbologyProperty(SymbologyInfo info, String propertyName, Object value)
         throws IllegalArgumentException;
+    
+    /**
+     * Gets the active symbology name that is being used to render this
+     * graphic symbol.  An active symbology will use the settings that were
+     * set on the Symbology object that will define how the graphic is displayed
+     * @return the active symbology name
+     */
+    public SymbologyInfo getActiveSymbology();
+    
+    /**
+     * Sets the active symbology name that is being used to render this
+     * graphic symbol.  
+     * @param symbologyName the new active symbology
+     * @throws IllegalArgumentException if the symbology isn't supported
+     */
+    public void setActiveSymbology(SymbologyInfo info) throws IllegalArgumentException;
 }
