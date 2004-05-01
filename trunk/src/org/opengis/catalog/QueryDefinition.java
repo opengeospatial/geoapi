7fc6c75fdc734c58d43143ef9ece51f08e3aebe0
desruisseaux <desruisseaux@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2004-05-01 02:37:04 +0000

    Javadoc and formatting fixes

--- trunk/src/org/opengis/catalog/QueryDefinition.java
+++ trunk/src/org/opengis/catalog/QueryDefinition.java
@@ -8,6 +8,8 @@
  **
  *************************************************************************************************/
 package org.opengis.catalog;
+
+
 /**
  * In general, a query definition specifies a Boolean function of multiple binary-valued conditions.
  * This Boolean function of multiple conditions combines these conditions using AND, OR, and NOT
@@ -25,13 +27,16 @@ package org.opengis.catalog;
  *      Catalog Services </A> Specification 
  */
 public interface QueryDefinition {
-	/**
-	 * Determines whether a CatalogEntry fits this query 
-	 * @param entry A CatalogEntry to evaluate
-	 * @return Returns true if the CatalogEntry fits the query.  Otherwise False is returned
-	 * @UML inferred from Section 3.1.1.1 <i>Query Functions</i> in the
-	 *      <A HREF="http://www.opengis.org/docs/99-113.pdf">OGC Abstract
-	 *      Catalog Services </A> Specification 
-	 */
-	public boolean evaluate(CatalogEntry entry);
+    /**
+     * Determines whether a CatalogEntry fits this query.
+     *
+     * @param entry A CatalogEntry to evaluate.
+     * @return Returns <code>true</code> if the specified {@linkplain CatalogEntry catalog entry}
+     *                 fits the query. Otherwise <code>false</code> is returned.
+     *
+     * @UML inferred from Section 3.1.1.1 <i>Query Functions</i> in the
+     *      <A HREF="http://www.opengis.org/docs/99-113.pdf">OGC Abstract
+     *      Catalog Services </A> Specification 
+     */
+    public boolean evaluate(CatalogEntry entry);
 }
