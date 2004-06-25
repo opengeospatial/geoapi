d1ffdb3c8ac5645c3d7a1ff65b872bb19ea9df1d
desruisseaux <desruisseaux@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2004-06-25 19:23:02 +0000

    Added missing interface

---
+++ trunk/src/org/opengis/referencing/crs/SingleCRS.java
@@ -0,0 +1,60 @@
+/*$************************************************************************************************
+ **
+ ** $Id$
+ **
+ ** $Source$
+ **
+ ** Copyright (C) 2003 Open GIS Consortium, Inc. All Rights Reserved. http://www.opengis.org/Legal/
+ **
+ *************************************************************************************************/
+package org.opengis.referencing.crs;
+
+// OpenGIS direct dependencies
+import org.opengis.referencing.datum.Datum;
+import org.opengis.referencing.cs.CoordinateSystem;
+
+
+/**
+ * Abstract coordinate reference system, consisting of a single
+ * {@linkplain CoordinateSystem Coordinate System} and a single
+ * {@linkplain Datum Datum} (as opposed to {@linkplain CompoundCRS Compound CRS}).
+ *
+ * A coordinate reference system consists of an ordered sequence of coordinate system
+ * axes that are related to the earth through a datum. A coordinate reference system
+ * is defined by one datum and by one coordinate system. Most coordinate reference system
+ * do not move relative to the earth, except for engineering coordinate reference systems
+ * defined on moving platforms such as cars, ships, aircraft, and spacecraft.
+ *
+ * Coordinate reference systems are commonly divided into sub-types. The common classification
+ * criterion for sub-typing of coordinate reference systems is the way in which they deal with
+ * earth curvature. This has a direct effect on the portion of the earth's surface that can be
+ * covered by that type of CRS with an acceptable degree of error. The exception to the rule is
+ * the subtype "Temporal" which has been added by analogy.
+ *
+ * @UML abstract SC_CoordinateReferenceSystem
+ * @author ISO 19111
+ * @author <A HREF="http://www.opengis.org">OpenGIS&reg; consortium</A>
+ * @version <A HREF="http://www.opengis.org/docs/03-073r1.zip">Abstract specification 2.0</A>
+ *
+ * @see org.opengis.referencing.cs.CoordinateSystem
+ * @see org.opengis.referencing.datum.Datum
+ */
+public interface SingleCRS extends CoordinateReferenceSystem {
+    /**
+     * Returns the coordinate system.
+     *
+     * @return The coordinate system.
+     * @UML association usesCS
+     *
+     * @rename Expanded the "CS" abbreviation into "CoordinateSystem".
+     */
+    CoordinateSystem getCoordinateSystem();
+
+    /**
+     * Returns the datum.
+     *
+     * @return The datum.
+     * @UML association usesDatum
+     */
+    Datum getDatum();
+}
