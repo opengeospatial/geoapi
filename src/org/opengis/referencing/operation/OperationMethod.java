8479ff38d9e30f0983be18d8219eabca157f356e
jive <jive@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2004-09-07 23:27:52 +0000

    Changed to ParameterValueGroup rather than ParameterValue Array

--- src/org/opengis/referencing/operation/OperationMethod.java
+++ src/org/opengis/referencing/operation/OperationMethod.java
@@ -14,7 +14,7 @@ import java.util.Locale;
 
 // OpenGIS direct dependencies
 import org.opengis.referencing.IdentifiedObject;
-import org.opengis.parameter.GeneralOperationParameter;
+import org.opengis.parameter.ParameterValueGroupDescriptor;
 
 
 /**
@@ -66,5 +66,5 @@ public interface OperationMethod extends IdentifiedObject {
      * @return The parameters, or an empty array if none.
      * @UML association usesParameter
      */
-    GeneralOperationParameter[] getParameters();
+    ParameterValueGroupDescriptor getParameters();
 }
