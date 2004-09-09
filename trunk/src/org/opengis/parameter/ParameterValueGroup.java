c2ef57c96335dfe3b066b0c7de89311f399472f3
desruisseaux <desruisseaux@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2004-09-09 06:38:10 +0000

    Slight javadoc formatting

--- trunk/src/org/opengis/parameter/ParameterValueGroup.java
+++ trunk/src/org/opengis/parameter/ParameterValueGroup.java
@@ -93,14 +93,15 @@ public interface ParameterValueGroup extends GeneralParameterValue {
     List/*<GeneralParameterValue>*/ values();
     
     /**
-     * Returns the first value in this group for the specified {@linkplain Identifier#getCode
-     * identifier code}.
-     * 
+     * Returns the first value in this group for the specified
+     * {@linkplain Identifier#getCode identifier code}.
      * If no {@linkplain ParameterValue parameter value} is found for the given
-     * code, then this method search recursively in subgroups (if any). This convenience method
-     * provides a way to get and set parameter values by name. For example the following idiom
-     * fetches a floating point value for the <code>"false_easting"</code> parameter:
-     * <br><br>
+     * code, then this method search recursively in subgroups (if any).
+     * <P>
+     * This convenience method provides a way to get and set parameter values by
+     * name. For example the following idiom fetches a floating point value for
+     * the <code>"false_easting"</code> parameter:
+     * </P>
      * <blockquote><code>
      * double value = parameter("false_easting").{@linkplain ParameterValue#doubleValue() doubleValue()};
      * </code></blockquote>
@@ -119,12 +120,11 @@ public interface ParameterValueGroup extends GeneralParameterValue {
      * <p>
      * If an existing ParameterValue is already included:
      * <ul>
-     * <li>For maxOccurs == 1, the new parameter will replace the existing parameter.
-     * <li>For maxOccurs > 1, the new parameter will be added
-     * <li>If adding the new parameter will increase the numbe past what
+     *   <li>For maxOccurs == 1, the new parameter will replace the existing parameter.
+     *   <li>For maxOccurs > 1, the new parameter will be added
+     *   <li>If adding the new parameter will increase the numbe past what
      * is allowable by maxOccurs an InvalidParameterTypeException will be thrown.
      * </p>
-     * <p>
      * 
      * @param parameter New parameter to be added to this group
      * @throws InvalidParameterTypeException if adding this parameter
