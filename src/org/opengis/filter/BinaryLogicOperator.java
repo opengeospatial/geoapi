2a4ad1d68c1e903c4303666960dbadfcb6d533c6
cdillard <cdillard@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2004-09-20 22:24:13 +0000

    Aligned with SLD spec to allow two OR MORE children.

--- src/org/opengis/filter/BinaryLogicOperator.java
+++ src/org/opengis/filter/BinaryLogicOperator.java
@@ -1,17 +1,12 @@
 package org.opengis.filter;
 
 /**
- * "Abstract" super-interface for logical operators that accept two other
- * logical values as inputs.  Currently, the only two subclasses are And and Or.
+ * "Abstract" super-interface for logical operators that accept two or more
+ * other logical values as inputs.  Currently, the only two subclasses are And
+ * and Or.
  */
 public interface BinaryLogicOperator extends Filter {
-    /**
-     * Returns the first (left) operand of this logical operator.
-     */
-    public Filter getFilter1();
+    public int getChildCount();
 
-    /**
-     * Returns the second (right) operand of this logical operator.
-     */
-    public Filter getFilter2();
+    public Filter getChild(int i) throws IndexOutOfBoundsException;
 }
