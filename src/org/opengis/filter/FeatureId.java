dd33cd4ca64d44851c195b125d8c68ff6b5e13d1
cdillard <cdillard@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2004-09-20 22:25:06 +0000

    Fixed to align with new getID() member in Feature

--- src/org/opengis/filter/FeatureId.java
+++ src/org/opengis/filter/FeatureId.java
@@ -2,20 +2,12 @@ package org.opengis.filter;
 
 /**
  * Instances of this interface represent a filter that passes only for features
- * that have the feature ID given to this object.  This class deviates from the
- * OGC Filter specification somewhat since we allow our Feature objects to have
- * composite primary keys.  So a feature ID must be an array of objects.
+ * that have one of the IDs given to this object.
  */
 public interface FeatureId extends Filter {
     /**
-     * Returns the array of objects whose elements will be compared against the
-     * (possibly composite) primary key of features.  If a given feature type
-     * has only one attribute for its primary key, then the returned array may
-     * be of length one.
-     * <p>
-     * It is an error to attempt to execute a Filter where the array given to
-     * this method has a length that is different from the number of primary key
-     * columns of the features being filtered.
+     * Returns an array containing the IDs of Features that will pass this
+     * filter.
      */
-    public Object [] getFID();
+    public String [] getIDs();
 }
