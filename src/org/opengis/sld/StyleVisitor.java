3046cedc84bd7b60401f0d2698daca3d5be2432e
cdillard <cdillard@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2004-09-22 17:03:31 +0000

    *** empty log message ***

---
+++ src/org/opengis/sld/StyleVisitor.java
@@ -0,0 +1,47 @@
+package org.opengis.sld;
+
+/**
+ * Interface that can be implemented by objects that want to perform some action
+ * on style objects.  The visitor design pattern is used to avoid runtime type
+ * checking of objects that descend from a common superclass.  So instead of
+ * code that looks like this, with costly instanceof checks:
+ * <pre>
+ * Symbol s = ... ;
+ * if (s instanceof LineSymbol) {
+ *   // Do Line stuff
+ * }
+ * else if (s instanceof PointSymbol) {
+ *   // Do Point stuff
+ * }
+ * ...
+ * </pre>
+ * you can implement the <code>SymbolVisitor</code> interface and the object
+ * itself will invoke the appropriate method:
+ * <pre>
+ * class MyVisitor implements SymbolVisitor {
+ *   public Object visit(LineSymbol symbol, Object userData) {
+ *     // Do Line stuff
+ *   }
+ *   public Object visit(PointSymbol symbol, Object userData) {
+ *     // Do Point stuff
+ *   }
+ *   ...
+ * }
+ * ...
+ * Symbol s = ... ;
+ * MyVisitor visitor = new MyVisitor(...);
+ * Object o = s.accept(visitor, myData);
+ * </pre>
+ */
+public interface StyleVisitor {
+    public Object visit(LineSymbol    symbol, Object userData);
+    public Object visit(PointSymbol   symbol, Object userData);
+    public Object visit(PolygonSymbol symbol, Object userData);
+    public Object visit(TextSymbol    symbol, Object userData);
+
+    public Object visit(ExternalGraphic externalGraphic, Object userData);
+    public Object visit(Mark mark, Object userData);
+
+    public Object visit(LinePlacement  placement, Object userData);
+    public Object visit(PointPlacement placement, Object userData);
+}
