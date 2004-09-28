cd4d0a30c74cafd917b11cb890b89f8e679b737d
crossley <crossley@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2004-09-28 17:13:41 +0000

    first edition of these interfaces, for web rendering and map2d control

---
+++ src/org/opengis/go/display/canvas/map2d/Map2DController.java
@@ -0,0 +1,55 @@
+package org.opengis.go.display.canvas.map2d;
+
+import javax.units.Unit;
+
+import org.opengis.go.display.canvas.CanvasController;
+import org.opengis.go.geometry.BoundingRectangle;
+
+
+/**
+ * The <code>Map2DController</code> offers setter methods for properties
+ * that define a rectangular, 2D <code>Canvas</code>.
+ * 
+ * @see Map2D
+ * 
+ * @author crossley
+ */
+public interface Map2DController extends CanvasController {
+    
+    /**
+     * Sets the pixel width of the <code>Canvas</code>, provided this
+     * <code>Map2DController</code> is the active controller.
+     * @param newPixelWidth the new pixel width
+     */
+    void setPixelWidth(int newPixelWidth);
+    
+    /**
+     * Sets the pixel height of the <code>Canvas</code>, provided this
+     * <code>Map2DController</code> is the active controller.
+     * @param newPixelHeight the new pixel height
+     */
+    void setPixelHeight(int newPixelHeight);
+    
+    /**
+     * Sets the map width of the <code>Canvas</code>, provided this
+     * <code>Map2DController</code> is the active controller.
+     * @param newMapWidth the new map width
+     * @param widthUnit the <code>Unit</code> of the newMapWidth
+     */
+    void setMapWidth(double newMapWidth, Unit widthUnit);
+    
+    /**
+     * Sets the scale of the <code>Canvas</code>, provided this
+     * <code>Map2DController</code> is the active controller.
+     * @param newScale the new scale
+     */
+    void setScale(double newScale);
+    
+    /**
+     * Sets the bounding rectangle of the <code>Canvas</code>, provided this
+     * <code>Map2DController</code> is the active controller.
+     * @param newBounds
+     */
+    void setBoundingRectangle(BoundingRectangle newBounds);
+
+}
