056ff6d7088784c90c23c8e838e759457da4a963
Martin Desruisseaux <desruisseaux@users.noreply.github.com>
2004-09-29 23:51:24 +0000

    InternationalString now extends CharSequence. Note that this is a 1.4 dependency, but this dependency already exists elswhere in GeoAPI. We may remove it later if there is a strong demand for 1.3 support.

--- src/org/opengis/util/InternationalString.java
+++ src/org/opengis/util/InternationalString.java
@@ -18,14 +18,16 @@ import java.util.Locale;
  * This interface is used as a replacement for the {@link String} type whenever an attribute needs to be
  * internationalization capable.
  *
+ * <P>The {@linkplain Comparable natural ordering} is defined by the string in
+ * {@linkplain Locale#getDefault default locale}, as returned by {@link #toString()}.
+ * This string also defines the {@linkplain CharSequence character sequence}.</P>
+ *
  * @author <A HREF="http://www.opengis.org">OpenGIS&reg; consortium</A>
  * @version 2.0
  *
  * @see javax.xml.registry.infomodel.InternationalString
- *
- * @revisit Should extends {@link CharSequence} if we are allowed to depends upon J2SE 1.4.
  */
-public interface InternationalString {
+public interface InternationalString extends CharSequence, Comparable {
     /**
      * Returns this string in the given locale. If no string is available in the given locale,
      * then some default locale is used. The default locale is implementation-dependent. It
@@ -38,10 +40,12 @@ public interface InternationalString {
     String toString(Locale locale);
 
     /**
-     * Returns this string in the implementation default locale. Invoking this method is
-     * equivalent to invoking <code>{@linkplain #toString(Locale) toString}(null)</code>.
+     * Returns this string in the default locale. Invoking this method is equivalent to invoking
+     * <code>{@linkplain #toString(Locale) toString}({@linkplain Locale#getDefault})</code>. All
+     * methods from {@link CharSequence} operate on this string. This string is also used as the
+     * criterion for {@linkplain Comparable natural ordering}.
      *
-     * @return The string in the implementation default locale.
+     * @return The string in the default locale.
      */
     String toString();
 }
