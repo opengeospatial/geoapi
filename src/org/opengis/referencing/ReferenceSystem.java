3306360c3fbd71641a05565c0f2085091f4f5dd3
Martin Desruisseaux <desruisseaux@users.noreply.github.com>
2004-10-06 05:48:45 +0000

    Removed deprecated methods.

--- src/org/opengis/referencing/ReferenceSystem.java
+++ src/org/opengis/referencing/ReferenceSystem.java
@@ -46,19 +46,4 @@ public interface ReferenceSystem extends IdentifiedObject {
      */
 /// @UML (identifier="scope", obligation=OPTIONAL)
     InternationalString getScope();
-
-    /**
-     * Description of domain of usage, or limitations of usage, for which this
-     * (coordinate) reference system object is valid.
-     *
-     * @param  locale The desired locale for the scope to be returned, or <code>null</code>
-     *         for a scope in some default locale (may or may not be the
-     *         {@linkplain Locale#getDefault() system default}).
-     * @return The Coordinate reference system scope in the given locale, or <code>null</code> if
-     *         none. If no scope is available in the given locale, then some default locale is used.
-     *
-     * @deprecated Replaced by {@link #getScope()}.
-     */
-/// @UML (identifier="scope", obligation=OPTIONAL)
-    String getScope(Locale locale);
 }
