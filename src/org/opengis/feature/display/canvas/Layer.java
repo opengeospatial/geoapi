933b195fa3fbe25c8d2fac534e345cfdfc91b03a
cdillard <cdillard@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2004-11-24 19:23:04 +0000

    Modified listener interfaces so that they extend Java's EventListener interface.  (According to Sun's docs, all event listeners should extend this marker interface.)  Fixed FeatureCanvas and Layer to account for changes to the Feature and DataStore interfaces.  Removed out-of-date layer interfaces.

--- src/org/opengis/feature/display/canvas/Layer.java
+++ src/org/opengis/feature/display/canvas/Layer.java
@@ -1,39 +1,116 @@
 package org.opengis.feature.display.canvas;
 
-//import java.util.Iterator;
+import java.util.List;
 
-import org.opengis.feature.FeatureType;
+import org.opengis.feature.FeatureCollection;
+import org.opengis.metadata.Identifier;
+import org.opengis.metadata.extent.Extent;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import org.opengis.sld.FeatureStyle;
+import org.opengis.spatialschema.geometry.Envelope;
 
 /**
- * Abstract base class for a grouping of features drawn on a Canvas.  Every
+ * Abstract base class for a grouping of features drawn on a Canvas.  The
+ * members of this interface are derived from the WMS specification, version
+ * 1.1.1.  This interface differs slightly from the WMS Layer element in that
+ * it assumes that the data is a FeatureCollection.  Also, in this spec, each
  * <code>Layer</code> has a Z-order value that indicates the order in which
  * features are drawn on the <code>FeatureCanvas</code>.  Lower numbers draw
  * first.
+ * <p>
+ * The majority of the properties of a layer are not required to be mutable.
+ * The only exceptions are the current style and the Z-order, which must be
+ * changeable at runtime.  The list of child layers can be modified, but
+ * implementations may require that the list of children be fixed once the layer
+ * is displayed.
  */
 public interface Layer {
     /**
-     * Retrieves the type of features that are drawn in this layer.
-     */
-    public FeatureType getFeatureType();
-
-    /**
-     * Returns the style that is applied to features in this layer when none
-     * is provided by the caller of the API.
-     */
-    public FeatureStyle getDefaultStyle();
-
-    /**
-     * Sets the style that will be applied to features in this layer when none
-     * is provided by the caller of the API.
-     */
-    public void setDefaultStyle(FeatureStyle style);
-
-    /**
-     * Returns a name for this layer.  This name should be human readable and
-     * may be presented to the end user in a user interface.
+     * Returns the name of this Layer.  This is a human-readable name that
+     * may be presented to the user.  This may be null.
+     * QUESTION: Should this become InternationalString?
      */
     public String getName();
+
+    /**
+     * Returns a terse, unique name for this layer.
+     */
+    public String getTitle();
+
+    /**
+     * Returns a longer description of this Layer.  This is human-readable text
+     * that may, for example, be presented to the user in a panel that gives
+     * details about a layer.
+     * QUESTION: Should this become InternationalString?
+     */
+    public String getAbstract();
+
+    /**
+     * Returns a list of keywords that apply to this layer.  These may be used,
+     * for example, to allow the user to find which layers in the system apply
+     * a topic of interest.
+     * QUESTION: Should this become List<InternationalString>?
+     */
+    public List/*<String>*/ getKeywordList();
+
+    /**
+     * This indicates the coordinate reference system of the geometry of the
+     * features in this layer.  This can be set if either the geometry does not
+     * specify a coordinate reference system or the user wishes to override the
+     * coordinate reference system and treat the coordinates as though they were
+     * in this one.
+     */
+    public CoordinateReferenceSystem getCoordinateReferenceSystem();
+
+    /**
+     * Returns a bounding rectangle that indcates, in latitude and longitude
+     * coordinates, the extent of the features in this layer.  This can be null
+     * if this in unknown or hard to calculate.
+     */
+    public Envelope getLatLonBoundingBox();
+
+    /**
+     * Returns a bounding rectangle that indcates, in the feature's native
+     * coordinate system, the extent of the data.  This can be null if this is
+     * unknown or hard to calculate.
+     */
+    public Envelope getBoundingBox();
+
+    /**
+     * Similar to getBoundingBox(), this method returns the extents of the data,
+     * but includes vertical, spatial, and temporal extents as well.  This may
+     * be null if this is unknown or hard to calculate.
+     */
+    public Extent getExtent();
+
+    /**
+     * Returns an identifier for this layer.
+     */
+    public Identifier getIdentifer();
+
+    /**
+     * Returns a list (which may be modified) of subordinate layers.
+     */
+    public List/*<Layer>*/ getChildLayers();
+
+//// Elements NOT in the WMS spec follow
+
+    /**
+     * Returns the collection of features that will be portrayed in this layer.
+     * A null value is allowed and indicates that nothing will be drawn.
+     */
+    public FeatureCollection getFeatureCollection();
+
+    /**
+     * Returns the style to apply to features in this layer.  A null value is
+     * allowed, and indicates that the features should not be drawn.
+     */
+    public FeatureStyle getStyle();
+
+    /**
+     * Sets the style that will be applied to features in this layer.
+     */
+    public void setStyle(FeatureStyle style);
 
     /**
      * Returns the minimum z-order level that this layer can be moved to.
@@ -59,8 +136,9 @@ public interface Layer {
     public double getLevel();
 
     /**
-     * Returns an iterator that ranges over all features currently in this
-     * layer.  PENDING(CSD): Who would call this?  Can we remove it?
-    public Iterator getFeatures();
+     * Allows an object to register for events when one of the mutable
+     * properties of this layer has changed.  A FeatureCanvas may, for example,
+     * use this to receive notification when the style has changed.
      */
+    public void addLayerListener(LayerListener ll);
 }
