ba75c19ae54ecda21a1232da976d018a613fbed6
jive <jive@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2004-12-09 04:02:23 +0000

    Separated our URI for identity from params for configuration.

--- src/org/opengis/feature/DataStoreException.java
+++ src/org/opengis/feature/DataStoreException.java
@@ -11,7 +11,23 @@ package org.opengis.feature;
 
 import java.io.IOException;
 
+/**
+ * Indicates a problem with a DataStore, allows nested exception in an IOException.
+ * <p>
+ * Since this is a DataStore exception should we have a backpointer
+ * to the DataStore? Or at least an indication of which data store caused
+ * the problem?
+ * </p>
+ * 
+ * @author Martin
+ */
 public class DataStoreException extends IOException {
+    
+    /**
+     * serialVersionUID allowing cross compiler use of DataStoreException. 
+     */
+    private static final long serialVersionUID = -7266283714520657425L;
+
     public DataStoreException() {
         super();
     }
