10c648354a58b5bda7a591ad0510e9761e2b5ab1
jive <jive@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2004-12-09 18:15:51 +0000

    Removed invalid transaction assumption, sorry, added toString implementation.

--- trunk/src/org/opengis/feature/DataStoreEvent.java
+++ trunk/src/org/opengis/feature/DataStoreEvent.java
@@ -23,32 +23,49 @@ public class DataStoreEvent extends EventObject {
      * Comment for <code>serialVersionUID</code>
      */
     private static final long serialVersionUID = -1842329357109520014L;
+    
     public static final int ADD = 1;
     public static final int MODIFY = 0;
     public static final int DELETE = -1;
 
     private GenericName typeName;
-    private Transaction transaction;
     private int type;
-    public DataStoreEvent(DataStore dataStore, GenericName typeName, Transaction transaction, int type) {
+    
+    public DataStoreEvent(DataStore dataStore, GenericName typeName, int type) {
         super(dataStore);
         this.typeName = typeName;
         this.type = type;
     }
-
+    
     public DataStore getDataStore() {
         return (DataStore) super.getSource();
     }
 
+    // consider rename to getGenericName?
     public GenericName getTypeName() {
         return typeName;
     }
-
-    public Transaction getTransaction() {
-        return transaction;
-    }
-
+    /** One of ADD, MODIFY, DELETE indicating event type */
     public int getType() {
         return type;
+    }
+    /**
+     * Of the form "DataStore (dataStore.displayName|dataStore) (ADD|MODIFY|DELETE) Event"
+     */
+    public String toString() {
+        StringBuffer buf = new StringBuffer();
+        buf.append( "DataStore " );
+        DataStore dataStore = getDataStore();
+        if( dataStore != null ){
+            buf.append( dataStore.getDisplayName() != null ? dataStore.getDisplayName() : dataStore );
+        }
+        switch( type ){
+        case ADD: buf.append( " ADD" ); // I want my Java 5 enum class :-)
+        case MODIFY: buf.append( " MODIFY" ); 
+        case DELETE: buf.append( " DELETE" );
+        }
+        buf.append( " Event" );
+        
+        return buf.toString();
     }
 }
