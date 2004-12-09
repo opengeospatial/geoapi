f3cab1c78c925a466d3091c5b298ff57d855e6ed
jive <jive@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2004-12-09 17:44:25 +0000

    Lock Request and Response objects.

---
+++ trunk/src/org/opengis/feature/LockResponse.java
@@ -0,0 +1,131 @@
+package org.opengis.feature;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Represents a response for a lock request.
+ * <p>
+ * A lock is requested for a duration, except for the special case of a TRANSACTION_LOCK
+ * where the request is until the next commit or rollback.
+ * </p>
+ * <p>
+ * A successful lock request will result in an AuthorizationID being generated for you.
+ * This is a simple token you can keep until you need to work with the content again.
+ * Without this token you are prevented from work (at least until the duration is up).
+ * </p>
+ * <p>
+ * Of course the fact that this is simply a long right now may indicate its future
+ * replacement. I am going to try and have commit and lock opperations return a
+ * LockResult, if that does not meet with approval this object can become a collector
+ * in the style of JUnitTestResults.
+ * </p>
+ * @author Jody Garnett
+ */
+public class LockResponse {
+    
+    /** Special case used to indicate a lock lasting until the end of the transaction */
+    static final LockResponse TRANSACTION_LOCK_RESPONSE = new LockResponse();
+    
+    
+    /** Authorization tokens by DataStore */
+    Map/*<DataStore,String>*/ authorizationMap;
+    
+    /**
+     * Number of Features locked, or -1 if unknown.
+     */
+    int numberLocked;
+    
+    /**
+     * Constructor usable for use with lock( Filter ) under AUTO_COMMIT.
+     * 
+     * @param dataStore DataStore being locked
+     * @param token Authorization token for later opperations on locked Features
+     * @param numberLocked Number of Features successfully locked, or -1 for unknown
+     */
+    public LockResponse( DataStore dataStore, String token, int numberLocked ){
+        authorizationMap = new HashMap();
+        authorizationMap.put( dataStore, token );
+        this.numberLocked = numberLocked;
+    }
+    /** Constructor for use of with a commit() implementation.
+     * <p>
+     * The results of the lock requests will need to be gathered using:
+     * <ul>
+     * <li>increaseNumberLocked( amount )
+     * <li>addAuthorizationToken( dataStore, token )
+     * </ul>
+     * </p>
+     */
+    public LockResponse(){
+        authorizationMap = new HashMap();
+        numberLocked = 0;
+    }
+    /**
+     * Number of features successfully locked, or -1 if unknown
+     * <p>
+     * In the rare case where you need to report back on the number of successful locks
+     * you can issue a bunch of individual lock( FidFilter ) requests and check the
+     * number of features locked vs the number of features indicated in your FidFilter.
+     * </p>
+     * @return number of locked features or -1 if unknown.
+     */
+    public int getNumberLocked() {
+        return numberLocked;
+    }    
+    
+    /** Used to collect the results of a number of lock requests for commit() */
+    public void increaseNumberLocked( int amount ){
+        numberLocked += amount;
+    }
+    /**
+     * Add an additional authorization token to collected results for commit().
+     * <p>
+     * Note this abstraction does not allow the collection of more then one token per
+     * DataStore. This should not be an issue give our two workflows:
+     * <ul>
+     * <li>Transaction.AUTO_COMMIT: Each lock method returns a different LockResponse.
+     * <li>Transaction + FeatureRequest.TRANSACTION_LOCK: Sepcial Case object TRANSACTION_LOCK_RESPONSE is always returned
+     * <li>Transaction: Each lock method returns null, commit returns a single LockResponse.
+     * Given that a Transaction is supposed to gather everything up into a single commit a datastore
+     * should be capable of issuing a single authorization token (even if more then one of
+     * its FeatureCollections was involved in the transaction.
+     * </ul>
+     * </p>
+     */  
+    public void addAuthorization( DataStore dataStore, String token ){
+        authorizationMap.put( dataStore, token );
+    }
+    
+    /**
+     * Authorization token for indicated dataStore, or null if no locks known for dataStore.
+     * 
+     * @param dataStore DataStore to search for
+     * @return token, or null if a token for dataStore was unavailable
+     */
+    public String getAuthorization( DataStore dataStore ){
+        return (String) authorizationMap.get( dataStore );
+    }
+    
+    /** Set of locked DataStores */
+    public Set/*<DataStore>*/ getDataStores(){
+        return Collections.unmodifiableSet( authorizationMap.keySet() );
+    }
+    
+    /**
+     * Retrives the authorization token for the special case of only one DataStore being locked.
+     * <p>
+     * This is the most common situration, often client code will only work on one FeatureCollection
+     * at a time, or several FeatureCollections belonging to the same DataStore.
+     * </p>
+     * @return token, or null if a single token was unavailable.
+     */
+    public String getToken(){
+        if( authorizationMap.size() == 1 ){
+            return (String) authorizationMap.values().iterator().next();
+        }
+        return null;
+    }
+}
