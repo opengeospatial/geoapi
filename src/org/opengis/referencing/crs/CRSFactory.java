b5f0c5e652b90b772554afc8c634d0a5a429a53a
Martin Desruisseaux <desruisseaux@users.noreply.github.com>
2005-01-31 06:59:22 +0000

    Deprecated a 'createProjectedCRS' method and added a new proposal.

--- src/org/opengis/referencing/crs/CRSFactory.java
+++ src/org/opengis/referencing/crs/CRSFactory.java
@@ -11,10 +11,12 @@ package org.opengis.referencing.crs;
 
 // J2SE direct dependencies
 import java.util.Map;
+import java.util.Set;
 
 // OpenGIS dependencies
 import org.opengis.referencing.ObjectFactory;
 import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.NoSuchIdentifierException;
 import org.opengis.referencing.cs.AffineCS;
 import org.opengis.referencing.cs.CartesianCS;
 import org.opengis.referencing.cs.SphericalCS;
@@ -28,8 +30,13 @@ import org.opengis.referencing.datum.ImageDatum;
 import org.opengis.referencing.datum.TemporalDatum;
 import org.opengis.referencing.datum.VerticalDatum;
 import org.opengis.referencing.operation.Conversion;
+import org.opengis.referencing.operation.Projection;
 import org.opengis.referencing.operation.MathTransform;
+import org.opengis.referencing.operation.MathTransformFactory;
+import org.opengis.referencing.operation.OperationMethod;
 import org.opengis.parameter.GeneralParameterValue;
+import org.opengis.parameter.ParameterValueGroup;
+import org.opengis.parameter.ParameterDescriptorGroup; // For javadoc
 
 
 /**
@@ -78,6 +85,45 @@ public interface CRSFactory extends ObjectFactory {
     EngineeringCRS createEngineeringCRS(Map         properties,
                                         EngineeringDatum datum,
                                         CoordinateSystem    cs) throws FactoryException;
+    
+    /**
+     * Creates an image coordinate reference system. 
+     *
+     * @param  properties Name and other properties to give to the new object.
+     *         Available properties are {@linkplain ObjectFactory listed there}.
+     * @param  datum Image datum to use in created CRS.
+     * @param  cs The Cartesian or Oblique Cartesian coordinate system for the created CRS.
+     * @throws FactoryException if the object creation failed.
+     */
+    ImageCRS createImageCRS(Map   properties,
+                            ImageDatum datum,
+                            AffineCS      cs) throws FactoryException;
+
+    /**
+     * Creates a temporal coordinate reference system. 
+     *
+     * @param  properties Name and other properties to give to the new object.
+     *         Available properties are {@linkplain ObjectFactory listed there}.
+     * @param  datum Temporal datum to use in created CRS.
+     * @param  cs The Temporal coordinate system for the created CRS.
+     * @throws FactoryException if the object creation failed.
+     */
+    TemporalCRS createTemporalCRS(Map      properties,
+                                  TemporalDatum datum,
+                                  TimeCS           cs) throws FactoryException;
+
+    /**
+     * Creates a vertical coordinate reference system. 
+     *
+     * @param  properties Name and other properties to give to the new object.
+     *         Available properties are {@linkplain ObjectFactory listed there}.
+     * @param  datum Vertical datum to use in created CRS.
+     * @param  cs The Vertical coordinate system for the created CRS.
+     * @throws FactoryException if the object creation failed.
+     */
+    VerticalCRS createVerticalCRS(Map     properties,
+                                  VerticalDatum datum,
+                                  VerticalCS       cs) throws FactoryException;
 
     /**
      * Creates a geocentric coordinate reference system from a {@linkplain CartesianCS
@@ -121,19 +167,6 @@ public interface CRSFactory extends ObjectFactory {
     GeographicCRS createGeographicCRS(Map      properties,
                                       GeodeticDatum datum,
                                       EllipsoidalCS    cs) throws FactoryException;
-    
-    /**
-     * Creates an image coordinate reference system. 
-     *
-     * @param  properties Name and other properties to give to the new object.
-     *         Available properties are {@linkplain ObjectFactory listed there}.
-     * @param  datum Image datum to use in created CRS.
-     * @param  cs The Cartesian or Oblique Cartesian coordinate system for the created CRS.
-     * @throws FactoryException if the object creation failed.
-     */
-    ImageCRS createImageCRS(Map   properties,
-                            ImageDatum datum,
-                            AffineCS      cs) throws FactoryException;
 
     /**
      * Creates a derived coordinate reference system. If the transformation is an affine
@@ -165,7 +198,7 @@ public interface CRSFactory extends ObjectFactory {
      * 
      * @param  properties Name and other properties to give to the new object.
      *         Available properties are {@linkplain ObjectFactory listed there}.
-     *         Properties for the {@link Conversion} object to be created can be specified
+     *         Properties for the {@link Projection} object to be created can be specified
      *         with the <code>"conversion."</code> prefix added in front of property names
      *         (example: <code>"conversion.name"</code>).
      * @param  geoCRS Geographic coordinate reference system to base projection on.
@@ -183,49 +216,101 @@ public interface CRSFactory extends ObjectFactory {
      * 
      * @param  properties Name and other properties to give to the new object.
      *         Available properties are {@linkplain ObjectFactory listed there}.
-     *         Properties for the {@link Conversion} object to be created can be specified
+     *         Properties for the {@link Projection} object to be created can be specified
      *         with the <code>"conversion."</code> prefix added in front of property names
      *         (example: <code>"conversion.name"</code>).
      * @param  geoCRS Geographic coordinate reference system to base projection on.
-     * @param  projectionName The classification name for the projection to be created
+     * @param  classification The classification name for the projection to be created
      *         (e.g. "Transverse_Mercator", "Mercator_1SP", "Oblique_Stereographic", etc.).
-     * @param  parameterValues The parameter value to give to the projection. Should includes
+     * @param  parameters The parameter values to give to the projection. May includes
      *         "central_meridian", "latitude_of_origin", "scale_factor", "false_easting",
      *         "false_northing" and any other parameters specific to the projection.
      * @param  cs The coordinate system for the projected CRS.
      * @throws FactoryException if the object creation failed.
+     *
+     * @deprecated Replaced by {@link #createProjectedCRS(Map,GeographicCRS,ParameterValueGroup,CartesianCS)}
+     *             for concistency with the rest of the API, which work with {@link ParameterValueGroup}
+     *             rather than an array of {@link GeneralParameterValue}.
      */
-    ProjectedCRS createProjectedCRS(Map                          properties,
-                                    GeographicCRS                    geoCRS,
-                                    String                   projectionName,
-                                    GeneralParameterValue[] parameterValues,
-                                    CartesianCS                          cs) throws FactoryException;
+    ProjectedCRS createProjectedCRS(Map                     properties,
+                                    GeographicCRS               geoCRS,
+                                    String              classification,
+                                    GeneralParameterValue[] parameters,
+                                    CartesianCS                     cs) throws FactoryException;
 
     /**
-     * Creates a temporal coordinate reference system. 
+     * Creates a projected coordinate reference system from a set of parameters. The classification name
+     * is inferred either from the {@linkplain ParameterDescriptorGroup#getName parameter group name},
+     * or any other implementation dependent way.
+     * <br><br>
+     * The client must supply at least the <code>"semi_major"</code> and <code>"semi_minor"</code>
+     * parameters for cartographic projection transforms. Example:
+     *
+     * <blockquote><pre>
+     * ParameterValueGroup parameters = factory.{@linkplain #getDefaultProjectionParameters getDefaultProjectionParameters}("Transverse_Mercator");
+     * p.parameter("semi_major").setValue(6378137.000);
+     * p.parameter("semi_minor").setValue(6356752.314);
+     * ProjectedCRS crs = factory.createProjectedCRS(..., parameters, ...);
+     * </pre></blockquote>
      *
      * @param  properties Name and other properties to give to the new object.
      *         Available properties are {@linkplain ObjectFactory listed there}.
-     * @param  datum Temporal datum to use in created CRS.
-     * @param  cs The Temporal coordinate system for the created CRS.
+     *         Properties for the {@link Projection} object to be created can be specified
+     *         with the <code>"conversion."</code> prefix added in front of property names
+     *         (example: <code>"conversion.name"</code>).
+     * @param  geoCRS Geographic coordinate reference system to base projection on.
+     * @param  parameters The parameter values to give to the projection.
+     * @param  cs The coordinate system for the projected CRS.
      * @throws FactoryException if the object creation failed.
+     *
+     * @see #getDefaultProjectionParameters
      */
-    TemporalCRS createTemporalCRS(Map      properties,
-                                  TemporalDatum datum,
-                                  TimeCS           cs) throws FactoryException;
+    ProjectedCRS createProjectedCRS(Map                 properties,
+                                    GeographicCRS           geoCRS,
+                                    ParameterValueGroup parameters,
+                                    CartesianCS                 cs) throws FactoryException;
 
     /**
-     * Creates a vertical coordinate reference system. 
+     * Returns the default parameter values for a projection of the given classification.
+     * The classification may be the name of any operation method returned by the
+     * {@link #getAvailableProjections} method. A typical example is
+     * <code>"<A HREF="http://www.remotesensing.org/geotiff/proj_list/transverse_mercator.html">Transverse_Mercator</A>"</code>).
+     * <br><br>
+     * The {@link #createProjectedCRS(Map,GeographicCRS,ParameterValueGroup,CartesianCS)} method
+     * in this factory shall be able to infer the classification from the parameter group returned
+     * by this method. A possible (but not required) approach is to set the
+     * {@linkplain ParameterDescriptorGroup#getName parameter group name} to the classification
+     * name.
+     * <br><br>
+     * This method must create new parameter instances at every call.
+     * It is intented to be modified by the user before to be passed to
+     * <code>{@linkplain #createProjectedCRS(Map,GeographicCRS,ParameterValueGroup,CartesianCS)
+     * createProjectedCRS}(..., parameters, ...)</code>.
      *
-     * @param  properties Name and other properties to give to the new object.
-     *         Available properties are {@linkplain ObjectFactory listed there}.
-     * @param  datum Vertical datum to use in created CRS.
-     * @param  cs The Vertical coordinate system for the created CRS.
-     * @throws FactoryException if the object creation failed.
+     * @param  classification The case insensitive classification to search for.
+     * @return The default parameter values.
+     * @throws NoSuchIdentifierException if there is no projection registered for the specified classification.
+     *
+     * @see #getAvailableProjections
+     * @see #createProjectedCRS(Map,GeographicCRS,ParameterValueGroup,CartesianCS)
      */
-    VerticalCRS createVerticalCRS(Map     properties,
-                                  VerticalDatum datum,
-                                  VerticalCS       cs) throws FactoryException;
+    ParameterValueGroup getDefaultProjectionParameters(String classification) throws NoSuchIdentifierException;
+
+    /**
+     * Returns a set of all available {@linkplain Projection projection} methods. For each
+     * element in this set, the {@linkplain OperationMethod#getName operation method name} shall
+     * be the classification name to be recognized by the {@link #getDefaultProjectionParameters}
+     * method.
+     * <br><br>
+     * The set of available projections is implementation dependent, but is usually a subset of
+     * {@linkplain MathTransformFactory#getAvailableTransforms the set of available transforms}.
+     *
+     * @return All {@linkplain Projection projection} methods available in this factory.
+     *
+     * @see #getDefaultProjectionParameters
+     * @see #createProjectedCRS(Map,GeographicCRS,ParameterValueGroup,CartesianCS)
+     */
+    Set/*<OperationMethod>*/ getAvailableProjections();
 
     /**
      * Creates a coordinate reference system object from a XML string.
