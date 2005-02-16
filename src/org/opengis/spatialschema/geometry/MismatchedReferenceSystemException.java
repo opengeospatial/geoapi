72fdd0dacf88e54565f8fd5458186461b1821ac4
Martin Desruisseaux <desruisseaux@users.noreply.github.com>
2005-02-16 00:34:07 +0000

    Added MismatchedReferenceSystemException.

---
+++ src/org/opengis/spatialschema/geometry/MismatchedReferenceSystemException.java
@@ -0,0 +1,43 @@
+/**************************************************************************************************
+ **
+ ** $Id$
+ **
+ ** $Source$
+ **
+ ** Copyright (C) 2003 Open GIS Consortium, Inc. All Rights Reserved. http://www.opengis.org/Legal/
+ **
+ *************************************************************************************************/
+package org.opengis.spatialschema.geometry;
+
+
+/**
+ * Indicates that an object cannot be constructed because of a mismatch in the
+ * {@linkplain org.opengis.referencing.ReferenceSystem reference systems} of
+ * geometric components.
+ *
+ * @version 1.0
+ * @author <A HREF="http://www.opengis.org">OpenGIS&reg; consortium</A>
+ */
+public class MismatchedReferenceSystemException extends IllegalArgumentException {
+    /**
+     * Serial number for interoperability with different versions.
+     */
+    private static final long serialVersionUID = 6222334569692693273L;
+
+    /**
+     * Creates an exception with no message.
+     */
+    public MismatchedReferenceSystemException() {
+        super();
+    }
+    
+    /**
+     * Creates an exception with the specified message.
+     *
+     * @param  message The detail message. The detail message is saved for 
+     *         later retrieval by the {@link #getMessage()} method.
+     */
+    public MismatchedReferenceSystemException(final String message) {
+        super(message);
+    }
+}
