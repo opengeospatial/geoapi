8daea5d63fd8ec3abb12b35410176f1db51c7ccf
crossley <crossley@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2005-04-22 20:57:03 +0000

    added get***Symbolizer to the graphic methods changed DEFAULT_BEGIN_ARROW to DEFAULT_STROKE_BEGIN_ARROW same for end arrow

--- trunk/pending/org/opengis/go/display/primitive/GraphicScaledImage.java
+++ trunk/pending/org/opengis/go/display/primitive/GraphicScaledImage.java
@@ -12,9 +12,8 @@ package org.opengis.go.display.primitive;
 // J2SE direct dependencies
 import java.awt.image.RenderedImage;
 
-import org.opengis.spatialschema.geometry.DirectPosition;
-import org.opengis.spatialschema.geometry.Envelope;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.spatialschema.geometry.DirectPosition;
 
 
 /**
