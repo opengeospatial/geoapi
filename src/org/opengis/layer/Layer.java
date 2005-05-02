9022192553d9e4ff6cf3886d2399c98914e48004
crossley <crossley@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2005-05-02 17:26:22 +0000

    added javadoc changed array signatures to Lists

--- src/org/opengis/layer/Layer.java
+++ src/org/opengis/layer/Layer.java
@@ -67,34 +67,97 @@ public interface Layer {
     InternationalString getAbstract();
     
     /**
-     * @revisit DOCUMENT ME.
+     * Provides keywords to aid in catalogue searches.
+     * 
+     * @return this {@code Layer}'s KeywordList
      */
     @UML (identifier="KeywordList", specification=ISO_19128) // 7.2.4.6.4
-    InternationalString[] getKeywordList();
+    List<InternationalString> getKeywordList();
     
     /**
-     * @revisit DOCUMENT ME.
+     * Provides the {@code CoordinateReferenceSystem}s available to
+     * this {@code Layer}.  Implementations should inherit CRSs from
+     * parent {@code Layer}s.
+     * 
+     * @return this {@code Layer}'s {@code CoordinateReferenceSystem}s
      */
-    CoordinateReferenceSystem[] getCRSs();
+    @UML (identifier="CRS", specification=ISO_19128) // 7.2.4.6.7
+    List<CoordinateReferenceSystem> getCRSs();
     
-    Envelope[] getBoundingBoxes();
+    /**
+     * Provides the BoundingBoxes that specify the coordinate ranges for
+     * this {@code Layer}, as {@code Envelope}s.   Implementations should
+     * inherit BoundingBoxes from parent {@code Layer}s.
+     * 
+     * @return this {@code Layer}'s BoundingBox {@code Envelope}s
+     */
+    @UML (identifier="BoundingBox", specification=ISO_19128)  // 7.2.4.6.8
+    List<Envelope> getBoundingBoxes();
     
+    /**
+     * Provides the {@code Attribution} for this {@code Layer}, which 
+     * identifies the source of the geographic information used in this
+     * {@code Layer}.
+     * 
+     * @return this {@code Layer}'s {@code Attribution}
+     */
+    @UML (identifier="Attribution", specification=ISO_19128) // 7.2.4.6.12
     Attribution getAttribution();
     
-    AuthorityURL[] getAuthorityURLs();
+    /**
+     * Provides the {@code AuthorityURL}s named in this {@code Layer}'s
+     * {@code Identifier}s.
+     * @return this {@code Layer}'s {@code AuthorityURL}s
+     */
+    @UML (identifier="AuthorityURL", specification=ISO_19128) // 7.2.4.6.13
+    List<AuthorityURL> getAuthorityURLs();
         
-    Identifier[] getIdentifiers();
+    /**
+     * Provides the {@code Identifier}s containing ID numbers or labels
+     * defined by a particular {@code Authority}.   
+     * @return this {@code Layer}'s {@code Identifier}s
+     */
+    @UML (identifier="Identifier", specification=ISO_19128) // 7.2.4.6.13
+    List<Identifier> getIdentifiers();
     
-    MetadataURL[] getMetadataURLs();
+    /**
+     * Provides the {@code MetadataURL}s that offer detailed, standardized
+     * metadata about the data for this {@code Layer}.
+     * 
+     * @return this {@code Layer}'s {@code MetadataURL}s
+     */
+    @UML (identifier="MetadataURL", specification=ISO_19128) // 7.2.4.6.11
+    List<MetadataURL> getMetadataURLs();
     
     DataURL[] getDataURLs();
     
-    FeatureListURL[] getFeatureListURLs();
+    /**
+     * Provides the {@code FeatureURL}s that point to a list of features 
+     * represented in this {@code Layer}.
+     * 
+     * @return this {@code Layer}'s {@code MetadataURL}s
+     */
+    @UML (identifier="MetadataURL", specification=ISO_19128) // 7.2.4.6.14
+    List<FeatureListURL> getFeatureListURLs();
     
-    Style[] getStyles();
-    
+    /**
+     * Provides the {@code Style}s that may be requested for this {@code Layer}.
+     * 
+     * @return this {@code Layer}'s {@code Style}s
+     */
+    @UML (identifier="Style", specification=ISO_19128) // 7.2.4.6.5
+    List<Style> getStyles();
+        
+    /**
+     * @return
+     */
+    @UML (identifier="MinScaleDenominator", specification=ISO_19128) // 7.2.4.6.9 
     double getMinScaleDenominator();
     
+    /**
+     * @return
+     */
+    @UML (identifier="MaxScaleDenominator", specification=ISO_19128) // 7.2.4.6.9
     double getMaxScaleDenominator();
 
     /**
@@ -106,14 +169,34 @@ public interface Layer {
     
     boolean isQueryable();
     
+    /**
+     * @return how many times this {@code Layer} has been cascaded
+     */
+    @UML (identifier="cascaded", specification=ISO_19128) // 7.2.4.7.3
     int getCascaded();
     
+    /**
+     * @return {@code Boolean.TRUE} if this {@code Layer}'s data is mostly opaque
+     */
+    @UML (identifier="opaque", specification=ISO_19128) // 7.2.4.7.4
     boolean isOpaque();
     
+    /**
+     * @return
+     */
+    @UML (identifier="noSubsets", specification=ISO_19128) // 7.2.4.7.5
     boolean isNoSubsets();
     
+    /**
+     * @return
+     */
+    @UML (identifier="fixedWidth", specification=ISO_19128) // 7.2.4.7.5
     int getFixedWidth();
     
+    /**
+     * @return
+     */
+    @UML (identifier="fixedHeight", specification=ISO_19128) // 7.2.4.7.5
     int getFixedHeight();
     
     //*************************************************************************
@@ -134,19 +217,24 @@ public interface Layer {
     /**
      * Gets the <code>FeatureLayer</code> from this {@code Layer} that is suitable
      * for adding to a <code>FeatureCanvas</code> in order to visually represent this
-     * {@code Layer}.
-     * @return the <code>FeatureLayer</code> to add to a <code>FeatureCanvas</code> or
-     *         null
+     * {@code Layer}. The returned {@code List} should NOT be a live list of this
+     * {@code Layer}'s {@code FeatureLayer}s, and modifying the list should not
+     * affect this {@code Layer}'s set of {@code FeatureLayer}s.
+     * 
+     * @return the <code>FeatureLayer</code>s to add to a <code>FeatureCanvas</code>
      */
-    FeatureLayer[] getFeatureLayers();
+    List<FeatureLayer> getFeatureLayers();
     
     /**
      * Gets the <code>Graphic</code>s from this {@code Layer} that are suitable
      * for adding to a <code>Canvas</code> in order to visually represent this 
-     * {@code Layer}.
-     * @return the <code>Graphic</code>s to add to a <code>Canvas</code> or an empty array.
+     * {@code Layer}.  The returned {@code List} should NOT be a live list of 
+     * this {@code Layer}'s {@code Graphic}s, and modifying the list should not
+     * affect this {@code Layer}'s set of {@code Graphic}s.
+     * 
+     * @return the <code>Graphic</code>s to add to a <code>Canvas</code> 
      */
-    Graphic[] getGraphics();
+    List<Graphic> getGraphics();
     
     
     
