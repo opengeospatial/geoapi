1595e6e8abf5042314d7b54a3f2582c9a57174f6
desruisseaux <desruisseaux@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2005-05-30 09:53:43 +0000

    Removed a @revisit tag that doesn't seems to apply anymore: its look like that OGC will not remove the SingleOperation class.

--- trunk/src/org/opengis/referencing/operation/SingleOperation.java
+++ trunk/src/org/opengis/referencing/operation/SingleOperation.java
@@ -22,11 +22,6 @@ import static org.opengis.annotation.Specification.*;
  * @version <A HREF="http://portal.opengeospatial.org/files/?artifact_id=6716">Abstract specification 2.0</A>
  * @author Martin Desruisseaux (IRD)
  * @since GeoAPI 1.0
- *
- * @revisit OpenGIS is considering to remove this class from UML diagrams. In this case. The
- *          removal of {@code SingleOperation} interface would simplify the API and brings more
- *          flexibility: {@link ConcatenatedOperation} would be a legal component in both
- *          other {@link ConcatenatedOperation} and in {@link PassThroughOperation}.
  */
 @UML(identifier="CC_SingleOperation", specification=ISO_19111)
 public interface SingleOperation extends CoordinateOperation {
