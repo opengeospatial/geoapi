b893a8b9c4378278176b566ae95376627a2826a7
Desruisseaux <Desruisseaux@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2006-02-23 03:07:07 +0000

    Trimmed some directories that are not used anymore.

--- scripts/xmi2java.bat
+++
@@ -1,7 +0,0 @@
-set SAXONHOME=c:\javastuff\saxon_6.5.2
-set GO1HOME=c:\go1
-del umlxml.xml
-java -jar %SAXONHOME%\saxon.jar -o umlxml.xml %1 %GO1HOME%\tools\newxmi2umlxml.xsl
-java -jar %SAXONHOME%\saxon.jar -o umlxml.jml umlxml.xml %GO1HOME%\tools\newUMLSchema2Java.xsl
-java -jar %SAXONHOME%\saxon.jar umlxml.jml %GO1HOME%\tools\Java_Files_Out.xsl
-
