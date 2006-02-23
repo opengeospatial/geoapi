93c4e55cedb332e52e27cf7d74a228070df82e15
Desruisseaux <Desruisseaux@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2006-02-23 08:48:03 +0000

    Get the 'pending' module to compile. We copied all interfaces from trunk (so we can modify them) except packages 'feature' because it seems completly changed, and packages 'go' and 'layer' (for now) because the depends on the old 'feature' package on the stable branch.

---
+++ trunk/pending/src/main/java/org/opengis/referencing/cs/CoordinateSystemAxis.java
@@ -0,0 +1,78 @@
+/*$************************************************************************************************
+ **
+ ** $Id$
+ **
+ ** $Source$
+ **
+ ** Copyright (C) 2003-2005 Open GIS Consortium, Inc.
+ ** All Rights Reserved. http://www.opengis.org/legal/
+ **
+ *************************************************************************************************/
+package org.opengis.referencing.cs;
+
+// Direct dependencies
+import javax.units.Unit;
+import org.opengis.referencing.IdentifiedObject;
+
+// Annotations
+import org.opengis.annotation.UML;
+import static org.opengis.annotation.Obligation.*;
+import static org.opengis.annotation.Specification.*;
+
+
+/**
+ * Definition of a coordinate system axis.
+ * See <A HREF="package-summary.html#AxisNames">axis name constraints</A>.
+ *
+ * @version <A HREF="http://portal.opengeospatial.org/files/?artifact_id=6716">Abstract specification 2.0</A>
+ * @author ISO/DIS 19111
+ * @author Martin Desruisseaux (IRD)
+ * @since GeoAPI 1.0
+ *
+ * @see CoordinateSystem
+ * @see Unit
+ */
+@UML(identifier="CS_CoordinateSystemAxis", specification=ISO_19111)
+public interface CoordinateSystemAxis extends IdentifiedObject {
+    /**
+     * The abbreviation used for this coordinate system axes. This abbreviation is also
+     * used to identify the ordinates in coordinate tuple. Examples are "<var>X</var>"
+     * and "<var>Y</var>".
+     *
+     * @return The coordinate system axis abbreviation.
+     */
+    @UML(identifier="axisAbbrev", obligation=MANDATORY, specification=ISO_19111)
+    String getAbbreviation();
+
+    /**
+     * Direction of this coordinate system axis. In the case of Cartesian projected
+     * coordinates, this is the direction of this coordinate system axis locally.
+     * Examples:
+     * {@linkplain AxisDirection#NORTH north} or {@linkplain AxisDirection#SOUTH south},
+     * {@linkplain AxisDirection#EAST  east}  or {@linkplain AxisDirection#WEST  west},
+     * {@linkplain AxisDirection#UP    up}    or {@linkplain AxisDirection#DOWN  down}.
+     * Within any set of coordinate system axes, only one of each pair of terms
+     * can be used. For earth-fixed coordinate reference systems, this direction is often
+     * approximate and intended to provide a human interpretable meaning to the axis. When a
+     * geodetic datum is used, the precise directions of the axes may therefore vary slightly
+     * from this approximate direction.
+     *
+     * Note that an {@link org.opengis.referencing.crs.EngineeringCRS} often requires
+     * specific descriptions of the directions of its coordinate system axes.
+     *
+     * @return The coordinate system axis direction.
+     */
+    @UML(identifier="axisDirection", obligation=MANDATORY, specification=ISO_19111)
+    AxisDirection getDirection();
+
+    /**
+     * The unit of measure used for this coordinate system axis. The value of this
+     * coordinate in a coordinate tuple shall be recorded using this unit of measure,
+     * whenever those coordinates use a coordinate reference system that uses a
+     * coordinate system that uses this axis.
+     *
+     * @return  The coordinate system axis unit.
+     */
+    @UML(identifier="axisUnitID", obligation=MANDATORY, specification=ISO_19111)
+    Unit getUnit();
+}
