149b407687a22f19b6b1a0cbec7a6107dbfd3000
sa5h <sa5h@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2006-03-10 20:06:14 +0000

    Changes (listed in jira task GEO-80) for migrating to ISO 19123 for Coverage.

---
+++ pending/src/main/java/org/opengis/coverage/CannotEvaluateException.java
@@ -0,0 +1,76 @@
+/*$************************************************************************************************
+ **
+ ** $Id: CannotEvaluateException.java 658 2006-02-22 18:09:34 -0700 (Wed, 22 Feb 2006) Desruisseaux $
+ **
+ ** $Source$
+ **
+ ** Copyright (C) 2003-2005 Open GIS Consortium, Inc.
+ ** All Rights Reserved. http://www.opengis.org/legal/
+ **
+ *************************************************************************************************/
+package org.opengis.coverage;
+
+// OpenGIS direct dependencies
+import org.opengis.spatialschema.geometry.DirectPosition;  // For Javadoc
+
+
+/**
+ * The base class for exceptions thrown when a quantity can't be evaluated.
+ * This exception is usually invoked by a
+ * <code>Coverage.{@linkplain Coverage#evaluate(DirectPosition, double[]) evaluate}(&hellip;)</code>
+ * method, for example when a point is outside the coverage.
+ *
+ * <P>&nbsp;</P>
+ * <TABLE WIDTH="80%" ALIGN="center" CELLPADDING="18" BORDER="4" BGCOLOR="#FFE0B0">
+ *   <TR><TD>
+ *     <P align="justify"><STRONG>WARNING: THIS CLASS WILL CHANGE.</STRONG> Current API is derived from OGC
+ *     <A HREF="http://www.opengis.org/docs/01-004.pdf">Grid Coverages Implementation specification 1.0</A>.
+ *     We plan to replace it by new interfaces derived from ISO 19123 (<CITE>Schema for coverage geometry
+ *     and functions</CITE>). Current interfaces should be considered as legacy and are included in this
+ *     distribution only because they were part of GeoAPI 1.0 release. We will try to preserve as much 
+ *     compatibility as possible, but no migration plan has been determined yet.</P>
+ *   </TD></TR>
+ * </TABLE>
+ *
+ * @author Martin Desruisseaux (IRD)
+ * @since GeoAPI 1.0
+ * @deprecated In favor of {@linkplain Coverage} from ISO 19123.
+ *
+ * @see Coverage#evaluate(DirectPosition, byte[])
+ * @see Coverage#evaluate(DirectPosition, double[])
+ */
+public class CannotEvaluateException extends RuntimeException {
+    /**
+     * Serial number for interoperability with different versions.
+     */
+    private static final long serialVersionUID = 506793649975583062L;
+
+    /**
+     * Creates an exception with no message.
+     */
+    public CannotEvaluateException() {
+        super();
+    }
+
+    /**
+     * Creates an exception with the specified message.
+     *
+     * @param  message The detail message. The detail message is saved for 
+     *         later retrieval by the {@link #getMessage()} method.
+     */
+    public CannotEvaluateException(String message) {
+        super(message);
+    }
+
+    /**
+     * Creates an exception with the specified message.
+     *
+     * @param  message The detail message. The detail message is saved for 
+     *         later retrieval by the {@link #getMessage()} method.
+     * @param  cause The cause for this exception. The cause is saved
+     *         for later retrieval by the {@link #getCause()} method.
+     */
+    public CannotEvaluateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
