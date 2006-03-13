4679188f85b06be0e8270a7d0bc085fc25ec211c
jdeolive <jdeolive@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2006-03-13 11:40:19 +0000

    made minimilast, removing convenience methods

--- pending/src/main/java/org/opengis/feature/AttributeFactory.java
+++ pending/src/main/java/org/opengis/feature/AttributeFactory.java
@@ -14,91 +14,71 @@ import org.opengis.spatialschema.geometry.Geometry;
 
 /**
  * Plays the role of making actual instances of types in this puzzle.
- * <p>
- * </p>
  * 
  * @author Gabriel Roldan, Axios Engineering
+ * @author Justin Deoliveira, The Open Planning Project
  *
  */
 public interface AttributeFactory {
 
+	/**
+	 * Creates a new attribute.
+	 * 
+	 * @param type The type of the attribute.
+	 * @param id The id of the attribute, may be null depending on the type.
+	 * @param value The initial value of the attribute, may be null depending on 
+	 * the type of the type of the attribute.
+	 * 
+	 */
+	Attribute create(AttributeType type, String id, Object value);
+	
+	/**
+	 * Creates a new geometry attribute.
+	 * 
+	 * @param type The type of the attribute. 
+	 * @param id The id of the attribute, may be null depending on the type.
+	 * @param crs The coordinate reference system of the attribute, may be null.
+	 * @param value The initial value of the attribute, may be null depending on 
+	 * the type of the type of the attribute.
+	 */
+	GeometryAttribute create(GeometryType type, String id, CoordinateReferenceSystem crs, Geometry value);
+	
+	/**
+	 * Creates a new complex attribute.
+	 * 
+	 * @param type The type of the attribute.
+	 * @param id The id of the attribute, may be null depending on the type.
+	 * @param value The initial value of the attribute, may be null depending on 
+	 * the type of the attribute.
+	 */
+	ComplexAttribute create(ComplexType type, String id, List value);
 
 	/**
-	 * Creates an attribute to hold values of the prescribed <code>type</code>
-	 * @param type
+	 * Creates a new feature.
+	 * 
+	 * @param type The type of the feature. 
+	 * @param id The id of the feature, (fid), may be null depending on the type.
+	 * @param value The initial value of the attribute, may be null depending on 
+	 * the type of the feature.
 	 */
-	public Attribute create(AttributeType type, String id);
+	Feature create(FeatureType type, String id, List value);
+	
+	/**
+	 * Creates a new simple feature.
+	 * 
+	 * @param type The type of the feature. 
+	 * @param id The id of the feature, (fid), may be null depending on the type.
+	 * @param value The initial value of the attribute, may be null depending on 
+	 * the type of the feature.
+	 */
+	SimpleFeature create(SimpleFeatureType type, String id, List value);
 
 	/**
+	 * Createsa a new feature collection.
 	 * 
-	 * @param type
-	 * @param value
+	 * @param type The type of the feature collection.
+	 * @param id The id of the feature collection, may be null depending on the 
+	 * type.
 	 */
-	public Attribute create(AttributeType type, String id, Object value);
-	
-	/**
-	 * 
-	 * @param type
-	 */
-	public GeometryAttribute create(GeometryType type, String id);
-	
-	/**
-	 * 
-	 * @param type
-	 * @param crs
-	 */
-	public GeometryAttribute create(GeometryType type, String id, CoordinateReferenceSystem crs);
-	
-	/**
-	 * 
-	 * @param type
-	 * @param value
-	 */
-	public GeometryAttribute create(GeometryType type, String id, Object value);
-	
-	/**
-	 * 
-	 * @param type
-	 * @param crs
-	 * @param value
-	 */
-	public GeometryAttribute create(GeometryType type, String id, CoordinateReferenceSystem crs, Geometry value);
-	
-	/**
-	 * 
-	 * @param type
-	 */
-	public SimpleFeature create(SimpleFeatureType type, String id);
-	
-	/**
-	 * 
-	 * @param type
-	 * @param values
-	 */
-	public SimpleFeature create(SimpleFeatureType type, String id, List<? extends Object> values);
-
-	/**
-	 * 
-	 * @param type
-	 * @param values
-	 */
-	public SimpleFeature create(SimpleFeatureType type, String id, Object[] values);
-
-	/**
-	 * 
-	 * @param type
-	 */
-	public ComplexAttribute create(ComplexType type, String id);
-
-	/**
-	 * 
-	 * @param type
-	 */
-	public Feature create(FeatureType type, String id);
-	
-	/**
-	 * 
-	 * @param type
-	 */
-	public FeatureCollection create(FeatureCollectionType type, String id);
+	FeatureCollection create(FeatureCollectionType type, String id);
 }
