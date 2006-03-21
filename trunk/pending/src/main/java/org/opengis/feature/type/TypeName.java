45adc1f4fc1ede50f9a49ed920b31931d72c48a8
jdeolive <jdeolive@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2006-03-21 19:23:53 +0000

    added a type name interface to compliement attribute name

---
+++ trunk/pending/src/main/java/org/opengis/feature/type/TypeName.java
@@ -0,0 +1,13 @@
+package org.opengis.feature.type;
+
+import org.opengis.feature.AttributeName;
+
+/**
+ * A scoped type name.
+ * 
+ * @author Justin Deoliveira, The Open Planning Project, jdeolive@openplans.org
+ *
+ */
+public interface TypeName extends AttributeName {
+
+}
