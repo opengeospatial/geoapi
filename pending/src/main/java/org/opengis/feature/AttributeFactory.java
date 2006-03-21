bf65a1629870d8a85c0a4169f3eacafd25ccc1b2
jdeolive <jdeolive@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2006-03-21 19:39:17 +0000

    attribute building methods

--- pending/src/main/java/org/opengis/feature/AttributeFactory.java
+++ pending/src/main/java/org/opengis/feature/AttributeFactory.java
@@ -1,9 +1,16 @@
 package org.opengis.feature;
 
+import java.util.Collection;
 import java.util.List;
 
+import org.opengis.feature.simple.BooleanAttribute;
+import org.opengis.feature.simple.NumericAttribute;
 import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureCollection;
 import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.simple.TemporalAttribute;
+import org.opengis.feature.simple.TextAttribute;
+import org.opengis.feature.type.AttributeDescriptor;
 import org.opengis.feature.type.AttributeType;
 import org.opengis.feature.type.ComplexType;
 import org.opengis.feature.type.FeatureCollectionType;
@@ -24,61 +31,125 @@ public interface AttributeFactory {
 	/**
 	 * Creates a new attribute.
 	 * 
-	 * @param type The type of the attribute.
-	 * @param id The id of the attribute, may be null depending on the type.
-	 * @param value The initial value of the attribute, may be null depending on 
-	 * the type of the type of the attribute.
-	 * 
+	 * @param descriptor The attribute descriptor.
+	 * @param id The id of the attribute, may be null depending on type.
+	 * @param value The value of the attribute, may be null depending on type.
 	 */
-	Attribute create(AttributeType type, String id, Object value);
+	Attribute createAttribute(AttributeDescriptor descriptor, String id , Object value);
+	
+	/**
+	 * Creates a new boolean attribute.
+	 * 
+	 * @param descriptor The attribute descriptor.
+	 * @param value The boolean value of the attribute.
+	 */
+	BooleanAttribute createBooleanAttribute(AttributeDescriptor descriptor, Boolean value);
+	
+	/**
+	 * Creates a new numberic attribute.
+	 * 
+	 * @param descriptor The attribute descriptor.
+	 * @param value The numeric value of the attribute.
+	 */
+	NumericAttribute createNumericAttribute(AttributeDescriptor descriptor, Number value);
+	
+	/**
+	 * Creates a new text attribute.
+	 * 
+	 * @param descriptor The attribute descriptor.
+	 * @param value The text value of the attribute.
+	 */
+	TextAttribute createTextAttribute(AttributeDescriptor descriptor, CharSequence value);
+	
+	/**
+	 * Creates a new temporal attribute.
+	 * 
+	 * @param descriptor Teh attribute descriptor.
+	 * @param value The calendar value of the attribute.
+	 */
+	TemporalAttribute createTemporalAttribute(AttributeDescriptor descriptor, String value);
 	
 	/**
 	 * Creates a new geometry attribute.
 	 * 
-	 * @param type The type of the attribute. 
+	 * @param desc The attribute descriptor.
 	 * @param id The id of the attribute, may be null depending on the type.
 	 * @param crs The coordinate reference system of the attribute, may be null.
 	 * @param value The initial value of the attribute, may be null depending on 
 	 * the type of the type of the attribute.
+	 * 
+	 * @throws IllegalArgumentException If desc.getType() does not return an 
+	 * instanceof {@link GeometryType}.
 	 */
-	GeometryAttribute create(GeometryType type, String id, CoordinateReferenceSystem crs, Geometry value);
+	GeometryAttribute createGeometryAttribute(
+		AttributeDescriptor desc, String id, CoordinateReferenceSystem crs, Object value
+	);
 	
 	/**
 	 * Creates a new complex attribute.
 	 * 
-	 * @param type The type of the attribute.
+	 * @param desc The attribute descriptor.
 	 * @param id The id of the attribute, may be null depending on the type.
 	 * @param value The initial value of the attribute, may be null depending on 
 	 * the type of the attribute.
+	 * 
+	 * @throws IllegalArgumentException If desc.getType() does not return an 
+	 * instanceof {@link ComplexType}.
 	 */
-	ComplexAttribute create(ComplexType type, String id, List value);
+	ComplexAttribute createComplexAttribute(
+		AttributeDescriptor desc, String id, Collection value
+	);
 
 	/**
 	 * Creates a new feature.
 	 * 
-	 * @param type The type of the feature. 
+	 * @param desc The attribute descriptor. 
 	 * @param id The id of the feature, (fid), may be null depending on the type.
 	 * @param value The initial value of the attribute, may be null depending on 
 	 * the type of the feature.
+	 * 
+	 * @throws IllegalArgumentException If desc.getType() does not return an 
+	 * instanceof {@link FeatureType}.
 	 */
-	Feature create(FeatureType type, String id, List value);
+	Feature createFeature(AttributeDescriptor desc, String id, Collection value);
 	
 	/**
 	 * Creates a new simple feature.
 	 * 
-	 * @param type The type of the feature. 
+	 * @param desc The attribute descriptor. 
 	 * @param id The id of the feature, (fid), may be null depending on the type.
 	 * @param value The initial value of the attribute, may be null depending on 
 	 * the type of the feature.
+	 * 
+	 * @throws IllegalArgumentException If desc.getType() does not return an 
+	 * instanceof {@link SimpleFeatureType}.
 	 */
-	SimpleFeature create(SimpleFeatureType type, String id, List value);
+	SimpleFeature createSimpleFeature(AttributeDescriptor desc, String id, List value);
 
 	/**
 	 * Createsa a new feature collection.
 	 * 
-	 * @param type The type of the feature collection.
+	 * @param desc The attribute descriptor.
 	 * @param id The id of the feature collection, may be null depending on the 
 	 * type.
+	 * 
+	 * @throws IllegalArgumentException If desc.getType() does not return an 
+	 * instanceof {@link FeatureCollectionType}.
 	 */
-	FeatureCollection create(FeatureCollectionType type, String id);
+	FeatureCollection createFeatureCollection(AttributeDescriptor desc, String id);
+
+	/**
+	 * Createsa a new simple feature collection.
+	 * 
+	 * @param desc The attribute descriptor.
+	 * @param id The id of the feature collection, may be null depending on the 
+	 * type.
+	 * 
+	 * @throws IllegalArgumentException If desc.getType() does not return an 
+	 * instanceof {@link org.opengis.feature.simple.SimpleFeatureCollectionType}.
+	 */
+	SimpleFeatureCollection createSimpleFeatureCollection(AttributeDescriptor desc, String id);
+	
+	
 }
+
