b94605af8c8431753afca366bcd7b864fd702dd4
jdeolive <jdeolive@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2006-03-21 19:27:33 +0000

    added interfaces for common simple attribute types

---
+++ trunk/pending/src/main/java/org/opengis/feature/simple/TemporalAttribute.java
@@ -0,0 +1,16 @@
+package org.opengis.feature.simple;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.opengis.feature.Attribute;
+
+/**
+ * Attribute containing a date value.
+ * 
+ * @author Justin Deoliveira, The Open Planning Project, jdeolive@openplans.org
+ *
+ */
+public interface TemporalAttribute extends Attribute<Date> {
+
+}
