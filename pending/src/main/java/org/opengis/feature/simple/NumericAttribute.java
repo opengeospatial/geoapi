8f400442bff50984d09a6dd7d55ee130208a44e7
jdeolive <jdeolive@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2006-03-21 19:27:33 +0000

    added interfaces for common simple attribute types

---
+++ pending/src/main/java/org/opengis/feature/simple/NumericAttribute.java
@@ -0,0 +1,13 @@
+package org.opengis.feature.simple;
+
+import org.opengis.feature.Attribute;
+
+/**
+ * Attribute containing a numeric value.
+ * 
+ * @author Justin Deoliveira, The Open Planning Project, jdeolive@openplans.org
+ *
+ */
+public interface NumericAttribute extends Attribute<Number> {
+
+}
---
+++ pending/src/main/java/org/opengis/feature/simple/TextAttribute.java
@@ -0,0 +1,13 @@
+package org.opengis.feature.simple;
+
+import org.opengis.feature.Attribute;
+
+/**
+ * Attribute containing a text value.
+ * 
+ * @author Justin Deoliveira, The Open Planning Project, jdeolive@openplans.org
+ *
+ */
+public interface TextAttribute extends Attribute<CharSequence> {
+
+}
