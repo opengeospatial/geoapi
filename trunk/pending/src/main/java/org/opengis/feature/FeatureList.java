1ce37b7cc97baeae2a98ae04dd7896fcd5923882
jive <jive@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2006-03-26 12:08:25 +0000

    improve javadocs, remove commented out methods

--- trunk/pending/src/main/java/org/opengis/feature/FeatureList.java
+++ trunk/pending/src/main/java/org/opengis/feature/FeatureList.java
@@ -8,13 +8,13 @@ import org.opengis.filter.Filter;
  * An ordered List of Features.
  * <p>
  * A FeatureList is usually retrived from a FeatureCollection with the
- * subCollection( Filter ) operation. How you ask - make use of Filter
- * 1.1 sortBy.
+ * subCollection( Filter ) operation using the Filter 1.1 construct SortBy.
  * </p>
  * <p>
  * You may check to see if the result of subCollection( Filter ) is
- * a FeatureList using an instanceof check. This often the case,
- * when using not using sortBy the order is usually based on FID.
+ * a FeatureList using an instanceof check. When not using an explicit
+ * sortBy parameter the order will be based on an internal index, often
+ * the one used to define the feature ID.
  * </p>
  * @author Jody Garnett, Refractions Research, Inc.
  */
