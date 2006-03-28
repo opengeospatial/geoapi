21a2df8d593213b430becff52195401b8e00dd1b
jdeolive <jdeolive@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2006-03-28 18:30:56 +0000

    added

---
+++ trunk/pending/src/main/java/org/opengis/feature/simple/SimpleFeatureBuilder.java
@@ -0,0 +1,11 @@
+package org.opengis.feature.simple;
+
+import java.util.List;
+
+import org.opengis.feature.Attribute;
+import org.opengis.feature.FeatureBuilder;
+
+public interface SimpleFeatureBuilder extends FeatureBuilder<List<Attribute>,SimpleFeatureType,SimpleFeature> {
+
+	
+}
