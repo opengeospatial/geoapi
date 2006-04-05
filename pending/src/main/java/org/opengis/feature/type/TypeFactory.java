1a82a0c33a94ed755f40d4a80e088f8cd78166e5
jdeolive <jdeolive@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2006-04-05 09:52:05 +0000

    factory methods for properties, operations

--- pending/src/main/java/org/opengis/feature/type/TypeFactory.java
+++ pending/src/main/java/org/opengis/feature/type/TypeFactory.java
@@ -19,34 +19,82 @@ import org.opengis.util.InternationalString;
  */
 public interface TypeFactory {
 
+	/**
+	 * Creates a new attribute descriptor.
+	 * 
+	 * @param type
+	 * 	The type of the described attribute.
+	 * @param name
+	 * 	The name of the described attribute. 
+	 * @param minOccurs
+	 * 	The minimum number of occurences of the described attribute.
+	 * @param maxOccurs
+	 * 	The maximum number of occurences of the described attribute.
+	 * @param isNillable
+	 * 	Flag indicating if the described attribute may have a null value.
+	 * 
+	 */
+	<T extends AttributeType>  
+	AttributeDescriptor<T> createAttributeDescriptor(
+		T type, Name name, int minOccurs, int maxOccurs, boolean isNillable
+	);
+	
+	/**
+	 * Creates a new association descriptor.
+	 * 
+	 * @param type
+	 * 	The type of the described association.
+	 * @param name
+	 * 	The name of the described association.
+	 * @param minOccurs
+	 * 	The minimum number of occurences of the described association.
+	 * @param maxOCcurs
+	 * 	The maximum number of occurences of the described association.
+	 * 
+	 */
+	<B extends AttributeType, A extends AssociationType<B>> 
+	AssociationDescriptor<A> createAssociationDescriptor(
+		A type, Name name, int minOccurs, int maxOCcurs
+	);
+	
+	/**
+	 * Creates a new operation descriptor.
+	 * 
+	 * @param type 
+	 * 	The type of the described operation.
+	 * @param isImplemented
+	 * 	Flag indicating if the described operation is implemented or not.
+	 */
+	<B, T extends AttributeType<B>, O extends OpperationType<B,T>>
+	OpperationDescriptor<B,T,O> createOpperationDescriptor(
+		O type, boolean isImplemented
+	);
+	
 	AssociationType createAssociationType(
-		TypeName name, Class association, boolean isIdentifiable, 
+		TypeName name, Class<AttributeType> association, boolean isIdentifiable, 
 		boolean isAbstract, Set<Filter> restrictions, AssociationType superType,
 		InternationalString description
 	);
 	
 	AttributeType createAttributeType(
-		TypeName name, Class binding, boolean isIdentifiable, 
-		boolean isAbstract, Set<Filter> restrictions, AttributeType superType,
-		InternationalString description
+		TypeName name, Class binding, boolean isIdentifiable, boolean isAbstract, 
+		Set<Filter> restrictions, AttributeType superType, InternationalString description
 	);
 	
 	ComplexType createComplexType(
-		TypeName name, Collection<AttributeDescriptor> schema, 
-		boolean isIdentifiable, boolean isAbstract, 
-		Set<Filter> restrictions, ComplexType superType,
+		TypeName name, Collection<StructuralDescriptor> schema, boolean isIdentifiable, 
+		boolean isAbstract, Set<Filter> restrictions, ComplexType superType,
 		InternationalString description
 	);
 	
 	GeometryType createGeometryType(
-		TypeName name, Class binding, CoordinateReferenceSystem crs,
-		boolean isIdentifiable, boolean isAbstract,  
-		Set<Filter> restrictions, AttributeType superType,
+		TypeName name, Class binding, CoordinateReferenceSystem crs, boolean isIdentifiable, 
+		boolean isAbstract, Set<Filter> restrictions, AttributeType superType,
 		InternationalString description
 	);
 
 	FeatureType createFeatureType(
-		TypeName name, Collection<AttributeDescriptor> schema,
+		TypeName name, Collection<StructuralDescriptor> schema,
 		AttributeDescriptor<GeometryType> defaultGeometry, CoordinateReferenceSystem crs,
 		boolean isAbstract, Set<Filter> restrictions, ComplexType superType,
 		InternationalString description
@@ -60,17 +108,16 @@ public interface TypeFactory {
 	);
 	
 	FeatureCollectionType createFeatureCollectionType(
-		TypeName name, Set<FeatureType> membersTypes, 
-		Collection<AttributeDescriptor> schema, 
-		AttributeDescriptor<GeometryType> defaultGeom, 
-		CoordinateReferenceSystem crs, boolean isAbstract, 
-		Set<Filter> restrictions, ComplexType superType,
+		TypeName name,  Collection<StructuralDescriptor> schema, 
+		AttributeDescriptor<GeometryType> defaultGeom, CoordinateReferenceSystem crs, 
+		boolean isAbstract, Set<Filter> restrictions, ComplexType superType,
 		InternationalString description
 	);
 	
-	SimpleFeatureCollectionType createSimpleFeatureCollectionType(
-		TypeName name, FeatureType memberType, CoordinateReferenceSystem crs, 
-		Set<Filter> restrictions, InternationalString description
-	);
+//	SimpleFeatureCollectionType createSimpleFeatureCollectionType(
+//		TypeName name, Collection<AssociationDescriptor> schema, CoordinateReferenceSystem crs, 
+//		boolean isAbstract, Set<Filter> restrictions, SimpleFeatureCollectionType superType,
+//		InternationalString description
+//	);
 
 }
