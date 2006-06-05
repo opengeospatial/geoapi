0a09c77a19f31183c9d89fec391e8610560ee2d2
Desruisseaux <Desruisseaux@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2006-06-05 08:33:32 +0000

    Javadoc clarification. Removed accidental inclusion.

--- trunk/geoapi/src/main/java/org/opengis/util/UnlimitedInteger.java
+++ trunk/geoapi/src/main/java/org/opengis/util/UnlimitedInteger.java
@@ -34,89 +34,89 @@ public final class UnlimitedInteger extends Number {
      */
     private static final long serialVersionUID = 4748246369364771836L;
 
-	/**
-	 * A constant holding the positive infinity.
-	 */
-	private final UnlimitedInteger POSITIVE_INFINITY = new UnlimitedInteger(Integer.MAX_VALUE);
+    /**
+     * A constant holding the positive infinity.
+     */
+    private final UnlimitedInteger POSITIVE_INFINITY = new UnlimitedInteger(Integer.MAX_VALUE);
 
-	/**
-	 * A constant holding the negative infinity.
-	 */
-	private final UnlimitedInteger NEGATIVE_INFINITY = new UnlimitedInteger(Integer.MIN_VALUE);
+    /**
+     * A constant holding the negative infinity.
+     */
+    private final UnlimitedInteger NEGATIVE_INFINITY = new UnlimitedInteger(Integer.MIN_VALUE);
 
     /**
      * The integer value.
      */
-	private final int value;
+    private final int value;
 
     /**
      * Constructs a newly allocated {@code UnlimitedInteger} object that represents the specified
      * {@code int} value. {@link Integer#MAX_VALUE} and {@link Integer#MIN_VALUE MIN_VALUE} maps
      * to positive and negative infinities respectively.
      */
-	public UnlimitedInteger(final int value) {
-		this.value = value;
-	}
+    public UnlimitedInteger(final int value) {
+        this.value = value;
+    }
 
     /**
      * Returns {@code true} if this integer represents a positive or negative infinity.
      */
-	public boolean isInfinite(){
-		return value == Integer.MAX_VALUE ||
+    public boolean isInfinite(){
+        return value == Integer.MAX_VALUE ||
                value == Integer.MIN_VALUE;
-	}
+    }
 
     /**
      * Returns the value of this {@code UnlimitedInteger} as an {@code int}. Positive and negative
      * infinities map to {@link Integer#MAX_VALUE} and {@link Integer#MIN_VALUE MIN_VALUE}
      * respectively.
      */
-	@Override
-	public int intValue() {
+    @Override
+    public int intValue() {
         return value;
-	}
+    }
 
     /**
      * Returns the value of this {@code UnlimitedInteger} as a {@code long}. Positive and negative
      * infinities map to {@link Long#MAX_VALUE} and {@link Long#MIN_VALUE MIN_VALUE} respectively.
      */
-	@Override
-	public long longValue() {
+    @Override
+    public long longValue() {
         return value;
-	}
+    }
 
     /**
      * Returns the value of this {@code UnlimitedInteger} as a {@code float}.
      * Positive and negative infinities map to {@link Float#POSITIVE_INFINITY}
      * and {@link Float#NEGATIVE_INFINITY NEGATIVE_INFINITY} respectively.
      */
-	@Override
-	public float floatValue() {
+    @Override
+    public float floatValue() {
         switch (value) {
             case Integer.MAX_VALUE: return Float.POSITIVE_INFINITY;
             case Integer.MIN_VALUE: return Float.NEGATIVE_INFINITY;
             default:                return value;
         }
-	}
+    }
 
     /**
      * Returns the value of this {@code UnlimitedInteger} as a {@code double}.
      * Positive and negative infinities map to {@link Double#POSITIVE_INFINITY}
      * and {@link Double#NEGATIVE_INFINITY NEGATIVE_INFINITY} respectively.
      */
-	@Override
-	public double doubleValue() {
+    @Override
+    public double doubleValue() {
         switch (value) {
             case Integer.MAX_VALUE: return Double.POSITIVE_INFINITY;
             case Integer.MIN_VALUE: return Double.NEGATIVE_INFINITY;
             default:                return value;
         }
-	}
+    }
 
     /**
      * Returns a string representation of this number.
      */
-	@Override
+    @Override
     public String toString() {
         switch (value) {
             case Integer.MAX_VALUE: return  "\u221E"; // Infinity symbol.
@@ -128,7 +128,7 @@ public final class UnlimitedInteger extends Number {
     /**
      * Returns a hash code for this integer.
      */
-	@Override
+    @Override
     public int hashCode() {
         return value;
     }
@@ -136,8 +136,8 @@ public final class UnlimitedInteger extends Number {
     /**
      * Compares this number with the specified object for equality.
      */
-	@Override
-	public boolean equals(final Object object) {
-		return (object instanceof UnlimitedInteger) && ((UnlimitedInteger) object).value == value;
-	}
+    @Override
+    public boolean equals(final Object object) {
+        return (object instanceof UnlimitedInteger) && ((UnlimitedInteger) object).value == value;
+    }
 }
