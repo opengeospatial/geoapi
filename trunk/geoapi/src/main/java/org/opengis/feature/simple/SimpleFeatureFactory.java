c625c9e411174c5482f930ec7a321c73c86e669f
jive <jive@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2006-06-07 11:45:16 +0000

    finished up factory interfaces

--- trunk/geoapi/src/main/java/org/opengis/feature/simple/SimpleFeatureFactory.java
+++ trunk/geoapi/src/main/java/org/opengis/feature/simple/SimpleFeatureFactory.java
@@ -1,6 +1,10 @@
 package org.opengis.feature.simple;
 
-import org.opengis.feature.AttributeFactory;
+import java.util.Date;
+
+import org.opengis.feature.Attribute;
+import org.opengis.feature.FeatureFactory;
+import org.opengis.feature.type.AttributeDescriptor;
 
 /**
  * This interface denotes a factory capable of producing SimpleFeature.
@@ -12,7 +16,71 @@ import org.opengis.feature.AttributeFactory;
  * 
  * @author Jody Garnett
  */
-public interface SimpleFeatureFactory extends AttributeFactory {
+public interface SimpleFeatureFactory extends FeatureFactory {
+	
+	/**
+	 * Creates a new attribute (always nested).
+	 * <p>
+	 * As currently defined this factory allows for the explicit creation of:
+	 * <ul>
+	 * <li>BooleanAttribute
+	 * <li>NumericAttribute
+	 * <li>TextAttribute
+	 * <li>TemporalAttribute
+	 * <li>GeometryAttribute (formal part of the model)
+	 * </ul>
+	 * 
+	 * @param value The value of the attribute, may be null depending on type.
+	 * @param descriptor The attribute descriptor.
+	 * @param id The id of the attribute, may be null depending on type.
+	 * 
+	 */
+	Attribute createAttribute(Object value, AttributeDescriptor descriptor, String id);
+	
+	/**
+	 * Creates a new boolean attribute (always nested).
+	 * 
+	 * XXX: Does this do more then shortcut getType().getBinding().isAssignableFrom( Boolean.class )?
+	 * 
+	 * @param value The boolean value of the attribute.
+	 * @param descriptor The attribute descriptor.
+	 * 
+	 */
+	BooleanAttribute createBooleanAttribute(Boolean value, AttributeDescriptor descriptor);
+	
+	/**
+	 * Creates a new numberic attribute (always nested).
+	 * 
+	 * XXX: Does this do more then shortcut getType().getBinding().isAssignableFrom( Number.class )?
+	 * 
+	 * @param value The numeric value of the attribute.
+	 * @param descriptor The attribute descriptor.
+	 * 
+	 */
+	NumericAttribute createNumericAttribute(Number value, AttributeDescriptor descriptor);
+	
+	/**
+	 * Creates a new text attribute (always nested).
+	 * 
+	 * XXX: Does this do more then shortcut getType().getBinding().isAssignableFrom( CharSequence.class )?
+	 * 
+	 * @param value The text value of the attribute.
+	 * @param descriptor The attribute descriptor.
+	 * 
+	 */
+	TextAttribute createTextAttribute(CharSequence value, AttributeDescriptor descriptor);
+	
+	/**
+	 * Creates a new temporal attribute (always nested).
+	 * 
+	 * XXX: Does this do more then shortcut getType().getBinding().isAssignableFrom( Date.class )?
+	 *
+	 * @param value The date value of the attribute.
+	 * @param descriptor Teh attribute descriptor.
+	 * 
+	 */
+	TemporalAttribute createTemporalAttribute(Date value, AttributeDescriptor descriptor);
+
 	/**
 	 * Creates a new simple feature.
 	 * 
