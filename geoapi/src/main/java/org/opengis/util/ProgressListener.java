b01c9ada19bfba30e75bab750d55f24c14c5ba48
Desruisseaux <Desruisseaux@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2006-08-13 09:49:09 +0000

    Replaced copyright header. First round on javadoc revision.

--- geoapi/src/main/java/org/opengis/util/ProgressListener.java
+++ geoapi/src/main/java/org/opengis/util/ProgressListener.java
@@ -1,25 +1,14 @@
-/*
- * Geotools 2 - OpenSource mapping toolkit
- * (C) 2003, Geotools Project Managment Committee (PMC)
- * (C) 2001, Institut de Recherche pour le D�veloppement
- * (C) 1999, P�ches et Oc�ans Canada
- *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License as published by the Free Software Foundation; either
- *    version 2.1 of the License, or (at your option) any later version.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
+/*$************************************************************************************************
+ **
+ ** $Id$
+ **
+ ** $URL$
+ **
+ ** Copyright (C) 2003-2005 Open GIS Consortium, Inc.
+ ** All Rights Reserved. http://www.opengis.org/legal/
+ **
+ *************************************************************************************************/
 package org.opengis.util;
-
 
 
 /**
@@ -28,71 +17,76 @@ package org.opengis.util;
  * This interface makes no assumption about the output device. Additionnaly, this
  * interface provides support for non-fatal warning and exception reports.
  * <p>
- * All implementations should be multi-thread safe, even the that provide
+ * All implementations should be multi-thread safe, even the ones that provide
  * feedback to a user interface thread.
  * <p>
  * Here an example showing how to cancel:
  * <pre><code>
  *      Iterator iterator = null;
- *      try{
+ *      try {
  *          float size = size();
  *          float position = 0;
  *          progress.started();
- *          for( iterator = iterator(); !progress.isCanceled() && iterator.hasNext(); progress.progress( (position++)/size )){
+ *          for (iterator = iterator(); !progress.isCanceled() && iterator.hasNext(); progress.progress(++position/size)) {
  *              try {
  *                  Feature feature = (Feature) iterator.next();
  *                  visitor.visit(feature);
  *              }
- *              catch( Exception erp ){
- *                  progress.exceptionOccurred( erp );
+ *              catch (Exception erp) {
+ *                  progress.exceptionOccurred(erp);
  *              }
  *          }
  *          progress.complete();
  *      }
  *      finally {
- *          close( iterator );
+ *          close (iterator);
  *      }
  * </code></pre>
  * Note the use of try and catch to report exceptions.
- * 
+ *
+ * @since  GeoAPI 2.1
  * @author Martin Desruisseaux
+ * @author Jody Garnet
  */
 public interface ProgressListener {
     /**
      * Description for the lengthly operation to be reported, or
      * {@code null} if none.
+     *
      * @deprecated Please use getTask().toString()
      */
     String getDescription();
-    
+
     /**
-     * Return the current task being performed, or null if none.
+     * Return the current task being performed, or {@code null} if none.
      * <p>
-     * It is assumed that if the task is null applications may simply report
+     * It is assumed that if the task is {@code null} applications may simply report
      * that the process is "in progress" or "working" as represented in the
-     * current locacle.
-     * </p>
+     * current locale.
+     *
      * @return task being performed.
      */
     InternationalString getTask();
-    
+
     /**
      * Set the current task being performed.
      * <p>
      * This task may be user visible and is represented as an
-     * InternationalStirng for locale support.
-     * </p>
+     * {@linkplain InternationalString international string} for locale support.
+     *
      * @param task
      */
-    void setTask( InternationalString task );
-    
+    void setTask(InternationalString task);
+
     /**
      * Set the description for the lenghtly operation to be reported. This method is usually
      * invoked before any progress begins. However, it is legal to invoke this method at any
      * time during the operation, in which case the description display is updated without
      * any change to the percentage accomplished.
-     * @deprecated please use setTask
+     *
      * @param description The new description, or {@code null} if none.
+     *
+     * @deprecated please use setTask
      */
     void setDescription(String description);
 
@@ -115,7 +109,7 @@ public interface ProgressListener {
     void complete();
 
     /**
-     * Release any resources used by this listener. If the progress were reported in a window,
+     * Releases any resources used by this listener. If the progress were reported in a window,
      * this window may be disposed.
      */
     void dispose();
@@ -124,17 +118,20 @@ public interface ProgressListener {
      * Is this job canceled?
      */
     boolean isCanceled();
-    
+
     /**
      * Indicate that progress should is canceled.
+     *
+     * @todo Is it really listener job to cancel a task?
      */
     void setCanceled(boolean cancel);    
-    
+
     /**
-     * Reports a warning. This warning may be printed to the {@linkplain System#err standard error
-     * stream}, appears in a windows or be ignored, at implementor choice.
+     * Reports a warning. This warning may be {@linkplain java.util.logger.Logger logged}, printed
+     * to the {@linkplain System#err standard error stream}, appears in a windows or be ignored,
+     * at implementor choice.
      *
-     * @param source The source of the warning, or {@code null} if none. This is typically the
+     * @param source Name of the warning source, or {@code null} if none. This is typically the
      *        filename in process of being parsed.
      * @param margin Text to write on the left side of the warning message, or {@code null} if none.
      *        This is typically the line number where the error occured in the {@code source} file.
