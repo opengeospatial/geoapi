edff30990a0a69f4f2ed57f11c956f919aef494e
Desruisseaux <Desruisseaux@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2006-12-16 18:33:57 +0000

    Resurrecting the 2.1-M1 branch, renamed as 2.1-M2.

---
+++ branches/2.1-Mx/geoapi/src/main/java/org/opengis/go/display/event/AggregationChangeEvent.java
@@ -0,0 +1,54 @@
+/**************************************************************************************************
+ **
+ ** $Id$
+ **
+ ** $URL$
+ **
+ ** Copyright (C) 2003-2005 Open GIS Consortium, Inc.
+ ** All Rights Reserved. http://www.opengis.org/legal/
+ **
+ *************************************************************************************************/
+package org.opengis.go.display.event;
+
+import org.opengis.go.display.primitive.Graphic;
+
+/**
+ * Provides a mechanism for notification of additions, removals, and
+ * reorderings of elements in an aggregation.
+ * 
+ * @version $Revision: 658 $, $Date: 2006-02-23 12:09:34 +1100 (jeu., 23 févr. 2006) $
+ * @author <A HREF="http://www.opengis.org">OpenGIS&reg; consortium</A>
+ */
+public interface AggregationChangeEvent {
+
+    /**
+     * Flag for one or more elements added to the aggregation.
+     */
+    public static final int ELEMENT_ADDED = 0;
+
+    /**
+     * Flag for one or more elements removed from the aggregation.
+     */
+    public static final int ELEMENT_REMOVED = 1;
+
+    /**
+     * Flag for the case of the element order changing within the aggregation.
+     */
+    public static final int ELEMENTS_REORDERED = 2;
+
+    /**
+     * Get the ID flag for this event.
+     *
+     * @return The event type. One of the {@link #ELEMENT_ADDED},
+     *         {@link #ELEMENT_REMOVED} or {@link #ELEMENTS_REORDERED}
+     *         constants.
+     */
+    public int getID();
+    
+    /**
+     * Gets the <code>Graphic</code> elements pertinent to the event.
+     * @return an array of <code>Graphic</code>s that were affected by the event,
+     *         or an empty array.
+     */
+    public Graphic[] getElements();
+}
---
+++ branches/2.1-Mx/geoapi/src/main/java/org/opengis/go/spatial/UnprojectedPathType.java
@@ -0,0 +1,61 @@
+/**************************************************************************************************
+ **
+ ** $Id$
+ **
+ ** $URL$
+ **
+ ** Copyright (C) 2003-2005 Open GIS Consortium, Inc.
+ ** All Rights Reserved. http://www.opengis.org/legal/
+ **
+ *************************************************************************************************/
+package org.opengis.go.spatial;
+
+/**
+ * This class serves to contain two constants: 
+ * <ol>
+ *   <li>
+ *     PIXEL_STRAIGHT, that indicates that a path between two points should
+ *     be drawn on the screen as a straight line, regardless of the location 
+ *     or orientation of the vertices.
+ *   </li>
+ *   <li>
+ *     CONTINUOUS_SPLINE, that indicates a spline is to be used to connect 
+ *     points along a path.
+ *   </li>
+ * </ol>
+ * These should be used in situations where accuracy is unimportant or the
+ * appearance of the path is intended to remain the same, regardless of the
+ * current viewport of the map.  Note that this applies to both
+ * two dimensional and three dimensional displays; in a three dimensional
+ * display, the path may cross other objects, but will always appear straight.
+ * 
+ * @author Open GIS Consortium, Inc.
+ * @version $Revision: 658 $, $Date: 2006-02-23 12:09:34 +1100 (jeu., 23 févr. 2006) $
+ */
+public class UnprojectedPathType extends PathType {
+
+    /**
+     * Creates a new UnprojectedPathType with the given value and name.
+     * @param name the short name for the enum.
+     * @param description the description for the enum.
+     */
+    protected UnprojectedPathType(String name, String description) {
+        super(name, description);
+    }
+    
+    public static final UnprojectedPathType PIXEL_STRAIGHT =
+        new UnprojectedPathType("PIXEL_STRAIGHT",
+            "The path that is drawn as screen-straight, regardless of any current projection.");
+            
+    public static final UnprojectedPathType CONTINUOUS_SPLINE =
+        new UnprojectedPathType("CONTINUOUS_SPLINE",
+            "The path that connects its points via a continuous (although not necessarily smooth) spline.");
+    
+    /** Enumeration value of the <code>PIXEL_STRAIGHT<\code> constant. */
+   // public static final int PIXEL_STRAIGHT_VALUE = PIXEL_STRAIGHT.getValue();
+    
+    /** Enumeration value of the <code>CONTINUOUS_SPLINE</code> constant. */
+   // public static final int CONTINUOUS_SPLINE_VALUE = CONTINUOUS_SPLINE.getValue();
+    
+
+}
