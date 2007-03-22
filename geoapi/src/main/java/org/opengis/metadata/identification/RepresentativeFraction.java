a79568c96712c1531b91d215bdb9216392bff404
jive <jive@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2007-03-22 17:59:09 +0000

    added doubleValue() to representativeFraction

--- geoapi/src/main/java/org/opengis/metadata/identification/RepresentativeFraction.java
+++ geoapi/src/main/java/org/opengis/metadata/identification/RepresentativeFraction.java
@@ -18,17 +18,25 @@ import static org.opengis.annotation.Specification.ISO_19115;
 /**
  * Derived from ISO 19103 Scale where MD_Representative Fraction.denominator = 1 / Scale.
  * Measure And Scale.targetUnits = Scale.sourceUnits
- *
+ * <p>
+ * Implementations are encouraged to extend Number.
+ * </p>
+ * 
  * @version <A HREF="http://www.opengis.org/docs/01-111.pdf">Abstract specification 5.0</A>
  * @author Ely Conn (Leica Geosystems Geospatial Imaging, LLC)
  * @since GeoAPI 2.1
  */
 @UML(identifier="MD_RepresentativeFraction", specification=ISO_19115)
-public interface RepresentativeFraction {
-
+public interface RepresentativeFraction {    
+    /**
+     * Returns this value in a form usable for computation.
+     * @return 1.0 / (double) getDenominator()
+     */
+    public double doubleValue();
+    
     /**
      * The number below the line in a vulgar fraction
      */
     @UML(identifier="denominator", obligation=MANDATORY, specification=ISO_19115)
-    int getDenominator();
+    int getDenominator(); // TODO: this may need to be a long? Source interface seems to indicate such
 }
