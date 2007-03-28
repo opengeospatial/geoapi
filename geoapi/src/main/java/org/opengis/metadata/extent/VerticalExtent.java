094e60391eda8316df064b02d1eb25c272e7a3bb
Martin Desruisseaux <desruisseaux@users.noreply.github.com>
2007-03-28 12:56:07 +0000

    Added explicit java.nio.charset.Charset name declarations.

--- geoapi/src/main/java/org/opengis/metadata/extent/VerticalExtent.java
+++ geoapi/src/main/java/org/opengis/metadata/extent/VerticalExtent.java
@@ -24,6 +24,7 @@ import static org.opengis.annotation.Specification.*;
  *
  * @version <A HREF="http://www.opengeospatial.org/standards/as#01-111">ISO 19115</A>
  * @author Martin Desruisseaux (IRD)
+ * @author Cory Horner (Refractions Research)
  * @since GeoAPI 1.0
  */
 @UML(identifier="EX_VerticalExtent", specification=ISO_19115)
