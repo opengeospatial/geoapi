d4830624b8a04a20d09cb0639991f19b671ede29
jive <jive@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2007-04-10 21:03:59 +0000

    now with code examples of interface design alternatives

---
+++ examples/src/main/java/org/opengis/example/publication/Citation.java
@@ -0,0 +1,9 @@
+package org.opengis.example.publication;
+
+import java.util.Collection;
+
+public interface Citation {
+    String getISBN();    
+    /**  @return an unmodifieable collection of CitationDate */
+    Collection<CitationDate> getDates();
+}
