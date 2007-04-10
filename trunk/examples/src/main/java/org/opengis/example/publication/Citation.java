6f422cebd023d45ab9f202778f43b74efda16188
jive <jive@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2007-04-10 22:53:58 +0000

    a few more examples

--- trunk/examples/src/main/java/org/opengis/example/publication/Citation.java
+++ trunk/examples/src/main/java/org/opengis/example/publication/Citation.java
@@ -3,7 +3,7 @@ package org.opengis.example.publication;
 import java.util.Collection;
 
 public interface Citation {
-    String getISBN();    
+    String getISBN();
     /**  @return an unmodifieable collection of CitationDate */
     Collection<CitationDate> getDates();
 }
