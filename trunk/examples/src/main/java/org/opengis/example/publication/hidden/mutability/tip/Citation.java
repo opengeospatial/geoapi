0dcd73e332e816067aca7e05f4fbab15bb0bf71d
jeichar <jeichar@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2007-04-10 21:37:16 +0000

    added rest of publication examples

---
+++ trunk/examples/src/main/java/org/opengis/example/publication/hidden/mutability/tip/Citation.java
@@ -0,0 +1,11 @@
+package org.opengis.example.publication.hidden.mutability.tip;
+
+import java.util.Collection;
+
+import org.opengis.example.publication.CitationDate;
+
+public interface Citation {
+    String getISBN();    
+    /**  @return an unmodifieable collection of CitationDate */
+    Collection<? extends CitationDate> getDates();
+}
