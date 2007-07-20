03db6961fdcca9ecab0da537f248d9f6d86a25bf
jdeolive <jdeolive@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2007-07-20 17:10:46 +0000

    made implement simple feature

--- trunk/geoapi/src/main/java/org/opengis/feature/simple/SimpleFeatureList.java
+++ trunk/geoapi/src/main/java/org/opengis/feature/simple/SimpleFeatureList.java
@@ -1,12 +1,12 @@
 package org.opengis.feature.simple;
 
-import java.util.Collection;
+import java.util.List;
 
 import org.opengis.feature.Attribute;
 import org.opengis.feature.FeatureList;
 
 public interface SimpleFeatureList<M extends SimpleFeatureType, T extends SimpleFeatureCollectionType<M>>
 	extends SimpleFeatureCollection<M,T>,
-	FeatureList<Attribute,Collection<Attribute>,M,T,SimpleFeature> {
+	FeatureList<Attribute,List<Attribute>,M,T,SimpleFeature> {
 
 }
