7925116b7951c95cc6283d5078f55262ef98e855
jdeolive <jdeolive@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2007-09-16 22:07:20 +0000

    association javadocs

--- geoapi/src/main/java/org/opengis/feature/type/AssociationDescriptor.java
+++ geoapi/src/main/java/org/opengis/feature/type/AssociationDescriptor.java
@@ -2,26 +2,19 @@ package org.opengis.feature.type;
 
 
 /**
- * Indicating a named association.
- * <p>
- * This class carries the ComplexType specific information requried
- * for using an association. Name, type and multiplicity are defined.
- * <p>
- * The goal of associations is to allow for a graph of data, this is contrast
- * to attribtues which indicate containement.
- * <p>
- * Please see the description of AssociationType for more guidelines on capturing
- * your data modeling needs with association.
- * </p>
+ * Describes an instance of an Association.
+ * 
  * @author Jody Garnett, Refractions Research
+ * @author Justin Deoliveira, The Open Planning Project
  */
 public interface AssociationDescriptor extends PropertyDescriptor {
 		
-	/**
-	 * Indicates the type of this association.
-	 * <p>
-	 * This indicates the relationship represented by the association.
-	 */
+    /** 
+     * Override of {@link PropertyDescriptor#getType()} which type narrows to 
+     * {@link AssocicationType}.
+     * 
+     *  @see PropertyDescriptor#getType()
+     */
 	AssociationType getType();
 
 }
