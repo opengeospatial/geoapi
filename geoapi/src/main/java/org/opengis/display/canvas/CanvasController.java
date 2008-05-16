34fa51ad77eec44be8857b5c3eb2f89fa822bf5d
Martin Desruisseaux <desruisseaux@users.noreply.github.com>
2008-05-16 19:04:26 +0000

    Javadoc fix, annotation and exception declaration.

--- geoapi/src/main/java/org/opengis/display/canvas/CanvasController.java
+++ geoapi/src/main/java/org/opengis/display/canvas/CanvasController.java
@@ -12,43 +12,50 @@ package org.opengis.display.canvas;
 
 import org.opengis.geometry.DirectPosition;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.TransformException;
 import org.opengis.util.InternationalString;
+import org.opengis.annotation.UML;
+
+import static org.opengis.annotation.Obligation.*;
+import static org.opengis.annotation.Specification.*;
 
 
 /**
- * The <code>CanvasController</code> holds all Setters
- * for the Canvas. Thoses methods are separate from the
- * getters (in the <code>CanvaState</code>) because a state
- * is an immutable object.
+ * Control the state of a {@linkplain Canvas canvas}, including its position, scale and title.
  *
- * @author Open Geospatial Consortium
+ * @author Open GIS Consortium, Inc.
  * @author Johann Sorel (Geomatys)
  * @since GeoAPI 2.2
  */
 public interface CanvasController {
-        
     /**
-     * Sets the canvas title.
-     * 
-     * @param title
-     */
-    void setTitle(InternationalString title);
-    
-    /**
-     * Sets the objective Coordinate Reference System (e.g. the projection of a
-     * georeferenced Coordinate Reference System) for this <code>Canvas</code>.
-     * This is the default objective Coordinate Reference System for this
-     * <code>Canvas</code>.
+     * Sets the title of the {@linkplain Canvas canvas}. The title of a may or may not be
+     * displayed on the titlebar of an application's window.
      *
-     * @param crs the objective Coordinate Reference System
+     * @param title the new title for the canvas.
      */
-    void setObjectiveCRS(CoordinateReferenceSystem crs);
-    
+    @UML(identifier="Canvas.setTitle", specification=OGC_03064, obligation=OPTIONAL)
+    void setTitle(InternationalString title);
+
     /**
-     * Set the center point of the Canvas.
-     * 
-     * @param newCenter
+     * Sets the objective Coordinate Reference System for this {@linkplain Canvas canvas}.
+     * This is for example the projection of a georeferenced Coordinate Reference System.
+     *
+     * @param  crs The new objective Coordinate Reference System.
+     * @throws TransformException If at least one graphic primivite can not be transformed to
+     *         the specified CRS, or if the given CRS can not be accepted for an other reason.
      */
-    void setCenter(DirectPosition newCenter);
-    
+    @UML(identifier="Canvas.setObjectiveCoordinateReferenceSystem", specification=OGC_03064, obligation=OPTIONAL)
+    void setObjectiveCRS(CoordinateReferenceSystem crs) throws TransformException;
+
+    /**
+     * Sets the position of the center pixel of the {@linkplain Canvas canvas} this
+     * controller works for.
+     *
+     * @param center The new center position.
+     *
+     * @see CanvasState#getCenter()
+     */
+    @UML(identifier="setCenter", specification=OGC_03064, obligation=OPTIONAL)
+    void setCenter(DirectPosition center);
 }
