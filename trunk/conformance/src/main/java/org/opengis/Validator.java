db9014d336675ad950714e0282edc60b065fd694
desruisseaux <desruisseaux@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2008-08-13 13:40:52 +0000

    Renamed compliance as conformance.

---
+++ trunk/conformance/src/main/java/org/opengis/Validator.java
@@ -0,0 +1,36 @@
+/*$************************************************************************************************
+ **
+ ** $Id$
+ **
+ ** $URL$
+ **
+ ** Copyright (C) 2004-2005 Open GIS Consortium, Inc.
+ ** All Rights Reserved. http://www.opengis.org/legal/
+ **
+ *************************************************************************************************/
+package org.opengis;
+
+import java.util.logging.Logger;
+import org.junit.Assert;
+
+
+/**
+ * Base class of all validators. A subclass is defined in mean packages.
+ *
+ * @author Martin Desruisseaux (Geomatys)
+ */
+public class Validator extends Assert {
+    /**
+     * The logger for reporting non-fatal warnings.
+     */
+    protected final Logger logger;
+
+    /**
+     * Creates a new validator instance.
+     *
+     * @param packageName The name of the package containing the classes to be validated.
+     */
+    protected Validator(final String packageName) {
+        logger = Logger.getLogger(packageName);
+    }
+}
