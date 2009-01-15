5bd12a414298fa1124a82c6d2cd9bb6950dbe937
desruisseaux <desruisseaux@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2009-01-15 20:03:45 +0000

    Replaced usage of a deprecated GridRange by GridEnvelope.

--- trunk/geoapi/src/main/java/org/opengis/coverage/grid/GridGeometry.java
+++ trunk/geoapi/src/main/java/org/opengis/coverage/grid/GridGeometry.java
@@ -50,7 +50,7 @@ public interface GridGeometry {
      * @return The valid coordinate range of a grid coverage.
      */
     @UML(identifier="gridRange", obligation=MANDATORY, specification=OGC_01004)
-    GridRange getGridRange();
+    GridEnvelope getGridRange();
 
     /**
      * Returns the conversion from grid coordinates to real world earth coordinates.
