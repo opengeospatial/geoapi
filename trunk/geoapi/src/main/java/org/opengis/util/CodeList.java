cbf8bfc00872a017666fb2768f318652dd025089
desruisseaux <desruisseaux@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2009-08-04 10:55:51 +0000

    Removed @Override on interfaces (merge from the patch applied by Jody on the 2.3-M3 tag).

--- trunk/geoapi/src/main/java/org/opengis/util/CodeList.java
+++ trunk/geoapi/src/main/java/org/opengis/util/CodeList.java
@@ -152,11 +152,11 @@ public abstract class CodeList<E extends CodeList<E>> implements Comparable<E>, 
         name = name.trim();
         final String n = name;
         return valueOf(codeType, new Filter() {
-            @Override public boolean accept(CodeList<?> code) {
+            public boolean accept(CodeList<?> code) {
                 return code.name().equals(n);
             }
 
-            @Override public String codename() {
+            public String codename() {
                 return n;
             }
         });
