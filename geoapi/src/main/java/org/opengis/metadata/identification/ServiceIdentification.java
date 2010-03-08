1f024965427a15d85cf0f07d85a554c491464d5f
Martin Desruisseaux <desruisseaux@users.noreply.github.com>
2010-03-08 11:07:59 +0000

    Reverted the previous change, and added a not explaining that the previx was renamed to 'SV_' in corrigendum 2006.

--- geoapi/src/main/java/org/opengis/metadata/identification/ServiceIdentification.java
+++ geoapi/src/main/java/org/opengis/metadata/identification/ServiceIdentification.java
@@ -18,10 +18,14 @@ import static org.opengis.annotation.Specification.*;
  * Identification of capabilities which a service provider makes available to a service user
  * through a set of interfaces that define a behaviour - See ISO 19119 for further information.
  *
+ * <BLOCKQUOTE><FONT SIZE=-1><B>NOTE:</B>
+ * The prefix was {@code MD} in a previous ISO 19115 specification, but
+ * has been renamed {@code SV} in corrigendum 2006.</FONT></BLOCKQUOTE>
+ *
  * @version <A HREF="http://www.opengeospatial.org/standards/as#01-111">ISO 19115</A>
  * @author  Martin Desruisseaux (IRD)
  * @since   GeoAPI 2.0
  */
-@UML(identifier="MD_ServiceIdentification", specification=ISO_19115)
+@UML(identifier="SV_ServiceIdentification", specification=ISO_19115)
 public interface ServiceIdentification extends Identification {
 }
