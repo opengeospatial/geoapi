775a944906877a28e06403a11cce33c615b9fa2d
Martin Desruisseaux <desruisseaux@users.noreply.github.com>
2010-06-30 03:41:53 +0000

    Updated the OGC copyright header as of section 2 in OGC 10-111 document. The updates were performed by an automatic script using the date of the first SVN commit as the starting year of each file. The results were reviewed manually. Deegree copyright (LGPL) were left unchanged - those files appear only in the pending section and may be removed in a future release.

--- geoapi-pending/src/main/java/org/opengis/webservice/capability/ServiceOperation.java
+++ geoapi-pending/src/main/java/org/opengis/webservice/capability/ServiceOperation.java
@@ -1,9 +1,3 @@
-/*
- * Created on 25.03.2004
- *
- * To change the template for this generated file go to
- * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
- */
 package org.opengis.webservice.capability;
 
 
