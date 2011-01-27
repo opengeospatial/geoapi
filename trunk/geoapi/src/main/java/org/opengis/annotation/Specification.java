dda956be4b51339a7576354e6b291ffd37e1b9e0
desruisseaux <desruisseaux@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2011-01-27 19:31:30 +0000

    Updated links to the specifications.

--- trunk/geoapi/src/main/java/org/opengis/annotation/Specification.java
+++ trunk/geoapi/src/main/java/org/opengis/annotation/Specification.java
@@ -34,6 +34,9 @@ package org.opengis.annotation;
 
 /**
  * The specifications from which an interface, method or code list was derived.
+ * When many sources are available for the same topic, the OGC
+ * <A HREF="http://www.opengeospatial.org/standards/as">Abstract Specifications</A>
+ * is preferred.
  *
  * @author  Martin Desruisseaux (IRD)
  * @author  Cédric Briançon (Geomatys)
@@ -42,54 +45,60 @@ package org.opengis.annotation;
  */
 public enum Specification {
     /**
-     * ISO 19103, Geographic information - Conceptual schema language.
+     * ISO 19103:2005, Geographic information - Conceptual schema language.
      * This is the specification for some interfaces in package {@link org.opengis.util}.
+     *
+     * @see <A HREF="http://www.iso.org/iso/iso_catalogue/catalogue_tc/catalogue_detail.htm?csnumber=37800">Buy from ISO</A>
      */
     ISO_19103,
 
     /**
-     * ISO 19107, Feature Geometry (Topic 1).
+     * ISO 19107:2003, Feature Geometry (Topic 1).
      * This is the specification for package {@link org.opengis.geometry} and sub-packages.
      *
-     * @see <A HREF="http://www.opengeospatial.org/standards/as">Buy from ISO</A>
+     * @see <A HREF="http://www.iso.org/iso/iso_catalogue/catalogue_tc/catalogue_detail.htm?csnumber=26012">Buy from ISO</A>
      */
     ISO_19107,
 
     /**
-     * ISO 19108, Temporal Schema.
+     * ISO 19108:2002, Temporal Schema.
      * This is the specification for package {@link org.opengis.temporal} and sub-packages.
+     *
+     * @see <A HREF="http://www.iso.org/iso/iso_catalogue/catalogue_tc/catalogue_detail.htm?csnumber=26013">Buy from ISO</A>
+     * @see <A HREF="http://www.iso.org/iso/iso_catalogue/catalogue_tc/catalogue_detail.htm?csnumber=44883">Corrigendum</A>
      */
     ISO_19108,
 
     /**
-     * ISO 19111, Spatial Referencing by Coordinates (Topic 2).
+     * ISO 19111:2007, Spatial Referencing by Coordinates (Topic 2).
      * This is the specification for package {@link org.opengis.referencing} and sub-packages.
      *
      * @see #OGC_01009
-     * @see <A HREF="http://www.opengeospatial.org/standards/as#04-046r3">Download from OGC</A>
+     * @see <A HREF="http://portal.opengeospatial.org/files/?artifact_id=39049">Download from OGC</A>
      */
     ISO_19111,
 
     /**
-     * ISO 19115, Metadata (Topic 11).
+     * ISO 19115:2003, Metadata (Topic 11).
      * This is the specification for package {@link org.opengis.metadata} and sub-packages.
      *
-     * @see <A HREF="http://www.opengeospatial.org/standards/as#01-111">Buy from ISO</A>
+     * @see <A HREF="http://www.iso.org/iso/iso_catalogue/catalogue_tc/catalogue_detail.htm?csnumber=26020">Buy from ISO</A>
+     * @see <A HREF="http://www.iso.org/iso/iso_catalogue/catalogue_tc/catalogue_detail.htm?csnumber=44361">Corrigendum</A>
      */
     ISO_19115,
 
     /**
-     * ISO 19115-2, Metadata part 2: extensions for imagery and gridded data.
+     * ISO 19115-2:2009, Metadata part 2: extensions for imagery and gridded data.
      * This is the specification for package {@link org.opengis.metadata} and sub-packages.
      *
-     * @todo add a link to the standard.
+     * @see <A HREF="http://www.iso.org/iso/iso_catalogue/catalogue_tc/catalogue_detail.htm?csnumber=39229">Buy from ISO</A>
      *
      * @since GeoAPI 2.3
      */
     ISO_19115_2,
 
     /**
-     * ISO 19117, Portrayal.
+     * ISO 19117:2005, Portrayal.
      * This is an abstract specification for portraying features.
      *
      * @see <A HREF="http://www.iso.org/iso/iso_catalogue/catalogue_tc/catalogue_detail.htm?csnumber=40395">Buy from ISO</A>
@@ -97,15 +106,16 @@ public enum Specification {
     ISO_19117,
 
     /**
-     * ISO 19123, Schema for coverage geometry and functions.
+     * ISO 19123:2005, Schema for coverage geometry and functions (Topic 6).
      * This is the specification for package {@link org.opengis.coverage} and sub-packages.
      *
      * @see #OGC_01004
+     * @see <A HREF="http://portal.opengeospatial.org/files/?artifact_id=19820">Download from OGC</A>
      */
     ISO_19123,
 
     /**
-     * ISO 19128, Layers and styles.
+     * ISO 19128:2005, Web map server interface.
      * This is the specification for package {@link org.opengis.layer} and sub-packages.
      *
      * @see <A HREF="http://portal.opengeospatial.org/files/?artifact_id=5316">Download from OGC</A>
@@ -113,10 +123,10 @@ public enum Specification {
     ISO_19128,
 
     /**
-     * ISO 19139, Metadata. XML schema implementation.
+     * ISO 19139:2007, Metadata. XML schema implementation.
      * This is the specification for package {@link org.opengis.metadata} and sub-packages.
      *
-     * @todo add a link to the standard.
+     * @see <A HREF="http://www.iso.org/iso/iso_catalogue/catalogue_tc/catalogue_detail.htm?csnumber=32557">Buy from ISO</A>
      *
      * @since GeoAPI 2.3
      */
@@ -156,8 +166,9 @@ public enum Specification {
      *
      * @see <A HREF="http://www.opengeospatial.org/standards/filter">Download from OGC</A>
      *
-     * @todo Need to be updated to {@code OGC 04-095}.
+     * @deprecated To be replaced by {@code OGC 09-026}.
      */
+    @Deprecated
     OGC_02059,
 
     /**
@@ -166,8 +177,9 @@ public enum Specification {
      *
      * @see <A HREF="http://www.opengeospatial.org/standards/sld">Download from OGC</A>
      *
-     * @todo Need to be updated to {@code OGC 05-078}.
+     * @deprecated To be replaced by {@code OGC 05-078}.
      */
+    @Deprecated
     OGC_02070,
 
     /**
@@ -175,27 +187,28 @@ public enum Specification {
      * This is the specification for package {@link org.opengis.feature} and sub-packages.
      *
      * @see <A HREF="http://www.opengeospatial.org/standards/wfs">Download from OGC</A>
+     *
+     * @deprecated To be replaced by {@code OGC 09-025}.
      */
+    @Deprecated
     OGC_04094,
 
     /**
      * Observations and Measurements, part 1.
      * This is the specification for package {@link org.opengis.observation}.
      *
-     * @see <A HREF="http://www.opengeospatial.org/standards/bp#05-087r4">Download from OGC</A>
-     *
-     * @todo Need to update the hyperlink.
+     * @deprecated To be replaced by Topic 20: Observations and Measurements
      */
+    @Deprecated
     OGC_07022,
 
     /**
      * Observations and Measurements, part 2: Sampling Features.
      * This is the specification for package {@link org.opengis.observation.sampling}.
      *
-     * @see <A HREF="http://www.opengeospatial.org/standards/bp#05-087r4">Download from OGC</A>
-     *
-     * @todo Need to update the hyperlink.
+     * @deprecated To be replaced by Topic 20: Observations and Measurements
      */
+    @Deprecated
     OGC_07002,
 
     /**
