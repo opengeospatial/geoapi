8162fe1925b9b8012747c5d564d6837b1752569e
Martin Desruisseaux <desruisseaux@users.noreply.github.com>
2011-08-12 17:27:50 +0000

    Initial set of ProjectedCRS tests in AuthorityFactoryTest.

--- geoapi-conformance/src/main/java/org/opengis/test/referencing/AuthorityFactoryTest.java
+++ geoapi-conformance/src/main/java/org/opengis/test/referencing/AuthorityFactoryTest.java
@@ -32,20 +32,28 @@
 package org.opengis.test.referencing;
 
 import java.util.List;
+import java.awt.geom.Rectangle2D;
 import javax.measure.unit.NonSI;
 
+import org.opengis.util.Factory;
+import org.opengis.util.FactoryException;
 import org.opengis.referencing.cs.*;
 import org.opengis.referencing.crs.*;
 import org.opengis.referencing.datum.*;
 import org.opengis.referencing.AuthorityFactory;
-import org.opengis.util.Factory;
-import org.opengis.util.FactoryException;
 import org.opengis.referencing.NoSuchAuthorityCodeException;
+import org.opengis.referencing.operation.TransformException;
+import org.opengis.referencing.operation.MathTransform;
+import org.opengis.referencing.operation.Conversion;
+import org.opengis.metadata.extent.Extent;
+import org.opengis.metadata.extent.GeographicExtent;
+import org.opengis.metadata.extent.GeographicBoundingBox;
+import org.opengis.test.TestCase;
+import org.opengis.test.ToleranceModifier;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.opengis.test.TestCase;
 
 import static org.junit.Assume.*;
 import static org.opengis.test.Assert.*;
@@ -141,8 +149,8 @@ public strictfp class AuthorityFactoryTest extends TestCase {
     public void testWGS84() throws NoSuchAuthorityCodeException, FactoryException {
         assumeNotNull(crsFactory);
         final GeographicCRS crs = crsFactory.createGeographicCRS("EPSG:4326");
+        assertNotNull("CRSAuthorityFactory.createGeographicCRS()", crs);
         validate(crs);
-        assertNotNull(crs);
         assertEquals("WGS 84", crs.getName().getCode());
         /*
          * Coordinate system validation.
@@ -164,5 +172,153 @@ public strictfp class AuthorityFactoryTest extends TestCase {
         final PrimeMeridian pm = datum.getPrimeMeridian();
         assertEquals(0.0, pm.getGreenwichLongitude(), 0.0);
         assertEquals(NonSI.DEGREE_ANGLE, pm.getAngularUnit());
+    }
+
+    /**
+     * Creates a {@link ProjectedCRS} identified by the given EPSG code, and tests its
+     * math transform. The set of allowed codes is documented in second column of the
+     * {@link PseudoEpsgFactory#createParameters(int)} method.
+     *
+     * @param  code The EPSG code of a target Coordinate Reference System.
+     * @param  swapλφ If the longitude and latitude axes shall be swapped.
+     * @param  swapxy If the easting and northing axes shall be swapped.
+     * @throws FactoryException If the math transform can not be created.
+     * @throws TransformException If a point can not be transformed.
+     */
+    private void runProjectionTest(final int code, final boolean swapλφ, final boolean swapxy)
+            throws FactoryException, TransformException
+    {
+        assumeNotNull(crsFactory);
+        final ProjectedCRS crs = crsFactory.createProjectedCRS("EPSG:" + code);
+        assertNotNull("CRSAuthorityFactory.createProjectedCRS()", crs);
+        validate(crs);
+        final Conversion conversion = crs.getConversionFromBase();
+        assertNotNull("ProjectedCRS.getConversionFromBase()", conversion);
+        final MathTransform projection = conversion.getMathTransform();
+        assertNotNull("Conversion.getMathTransform()", conversion);
+        final MathTransformTest test = new MathTransformTest(null);
+        test.transform = projection;
+        /*
+         * Get the sample points and swap ordinate values if needed.
+         */
+        final SamplePoints sample = SamplePoints.getSamplePoints(code);
+        if (swapλφ) SamplePoints.swap(sample.sourcePoints);
+        if (swapxy) SamplePoints.swap(sample.targetPoints);
+        if (swapλφ) {
+            test.toleranceModifier = ToleranceModifier.PROJECTION_φλ;
+        }
+        test.verifyKnownSamplePoints(sample);
+        /*
+         * Tests random points in every domains of validity declared in the CRS.
+         * If the CRS does not declare any domain of validity, then we will use
+         * the one which is hard-coded in the SamplePoints class.
+         */
+        boolean tested = false;
+        final Rectangle2D areaOfValidity = sample.areaOfValidity;
+        double λmin = areaOfValidity.getMinX();
+        double λmax = areaOfValidity.getMaxX();
+        double φmin = areaOfValidity.getMinY();
+        double φmax = areaOfValidity.getMaxY();
+        final Extent extent = crs.getDomainOfValidity();
+        validate(extent);
+        if (extent != null) {
+            for (final GeographicExtent element : extent.getGeographicElements()) {
+                if (element instanceof GeographicBoundingBox && Boolean.TRUE.equals(element.getInclusion())) {
+                    final GeographicBoundingBox bbox = (GeographicBoundingBox) element;
+                    λmin = bbox.getWestBoundLongitude();
+                    λmax = bbox.getEastBoundLongitude();
+                    φmin = bbox.getSouthBoundLatitude();
+                    φmax = bbox.getNorthBoundLatitude();
+                    if (swapλφ) areaOfValidity.setRect(φmin, λmin, φmax-φmin, λmax-λmin);
+                    else        areaOfValidity.setRect(λmin, φmin, λmax-λmin, φmax-φmin);
+                    assertFalse("Empty geographic bounding box.", areaOfValidity.isEmpty());
+                    test.verifyInDomainOfValidity(areaOfValidity, code);
+                    tested = true;
+                }
+            }
+        }
+        if (!tested) {
+            if (swapλφ) {
+                areaOfValidity.setRect(φmin, λmin, φmax-φmin, λmax-λmin);
+            }
+            test.verifyInDomainOfValidity(areaOfValidity, code);
+        }
+    }
+
+    /**
+     * Tests the EPSG:3002 ("<cite>Makassar / NEIEZ</cite>") projected CRS.
+     * This is a "<cite>Mercator (variant A)</cite>" projection with (&phi;,&lambda;)
+     * axis order in input and (<var>E</var>,<var>N</var>) axis order in output.
+     *
+     * @throws FactoryException If the math transform can not be created.
+     * @throws TransformException If the example point can not be transformed.
+     *
+     * @see MathTransformTest#testMercator1SP()
+     */
+    @Test
+    public void testEPSG_3002() throws FactoryException, TransformException {
+        runProjectionTest(3002, true, false);
+    }
+
+    /**
+     * Tests the EPSG:3388 ("<cite>Pulkovo 1942 / Caspian Sea Mercator</cite>") projected CRS.
+     * This is a "<cite>Mercator (variant B)</cite>" projection with (&phi;,&lambda;)
+     * axis order in input and (<var>N</var>,<var>E</var>) axis order in output.
+     *
+     * @throws FactoryException If the math transform can not be created.
+     * @throws TransformException If the example point can not be transformed.
+     *
+     * @see MathTransformTest#testMercator2SP()
+     */
+    @Test
+    public void testEPSG_3388() throws FactoryException, TransformException {
+        runProjectionTest(3388, true, true);
+    }
+
+    /**
+     * Tests the EPSG:3857 ("<cite>WGS 84 / Pseudo-Mercator</cite>") projected CRS.
+     * This is a "<cite>Mercator Popular Visualisation Pseudo Mercator</cite>"
+     * projection with (&phi;,&lambda;) axis order in input and (<var>E</var>,<var>N</var>)
+     * axis order in output.
+     *
+     * @throws FactoryException If the math transform can not be created.
+     * @throws TransformException If the example point can not be transformed.
+     *
+     * @see MathTransformTest#testPseudoMercator()
+     */
+    @Test
+    public void testEPSG_3857() throws FactoryException, TransformException {
+        runProjectionTest(3857, true, false);
+    }
+
+    /**
+     * Tests the EPSG:24200 ("<cite>JAD69 / Jamaica National Grid</cite>") projected CRS.
+     * This is a "<cite>Lambert Conic Conformal (1SP)</cite>" projection with (&phi;,&lambda;)
+     * axis order in input and (<var>E</var>,<var>N</var>) axis order in output.
+     *
+     * @throws FactoryException If the math transform can not be created.
+     * @throws TransformException If the example point can not be transformed.
+     *
+     * @see MathTransformTest#testLambertConicConformal1SP()
+     */
+    @Test
+    public void testEPSG_24200() throws FactoryException, TransformException {
+        runProjectionTest(24200, true, false);
+    }
+
+    /**
+     * Tests the EPSG:32040 ("<cite>NAD27 / Texas South Central</cite>") projected CRS.
+     * This is a "<cite>Lambert Conic Conformal (2SP)</cite>" projection with (&phi;,&lambda;)
+     * axis order in input and (<var>E</var>,<var>N</var>) axis order in output.
+     *
+     * @throws FactoryException If the math transform can not be created.
+     * @throws TransformException If the example point can not be transformed.
+     *
+     * @see MathTransformTest#testLambertConicConformal1SP()
+     */
+    @Test
+    @org.junit.Ignore // TODO: needs to implement conversion to feets.
+    public void testEPSG_32040() throws FactoryException, TransformException {
+        runProjectionTest(32040, true, false);
     }
 }
