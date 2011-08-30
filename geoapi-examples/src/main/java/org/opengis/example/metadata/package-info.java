b409e85105c1b244961ff41b07e695db0a6eeb53
Martin Desruisseaux <desruisseaux@users.noreply.github.com>
2011-08-30 16:49:52 +0000

    Provides example of metadata implementation using proxy.

--- geoapi-examples/src/main/java/org/opengis/example/metadata/package-info.java
+++ geoapi-examples/src/main/java/org/opengis/example/metadata/package-info.java
@@ -7,7 +7,15 @@
  */
 
 /**
- * Simple implementation of a few metadata objects.
+ * Implements all metadata interfaces through a simple {@link java.lang.reflect.Proxy}. This package
+ * demonstrates that it is possible to implement all interfaces in the {@link org.opengis.metadata}
+ * package and sub-packages with minimal effort. This implementation stores the metadata values in
+ * {@link java.util.Map} objects, but the same strategy can also be applied on a wide variety of
+ * storage mechanisms like JDBC connection or a LDAP protocol.
+ * <p>
+ * In addition to the proxy classes, this package provides an explicit implementation of the
+ * {@link org.opengis.metadata.citation.Citation} interface because it is widely used by the
+ * referencing packages.
  * <p>
  * Every classes in this package are hereby placed into the Public Domain.
  * This means anyone is free to do whatever they wish with those files.
