ca68163d2d077b592cee416395f101000c0274a8
Martin Desruisseaux <desruisseaux@users.noreply.github.com>
2011-11-07 20:37:59 +0000

    Moved the Proj.4 wrappers to public domain. We consider them as an example of GeoAPI implementation, in the hope to make easier for implementors to derive their own implementation.

--- geoapi-proj4/src/main/java/org/proj4/PJ.java
+++ geoapi-proj4/src/main/java/org/proj4/PJ.java
@@ -2,35 +2,15 @@
  *    GeoAPI - Java interfaces for OGC/ISO standards
  *    http://www.geoapi.org
  *
- *    Copyright (C) 2011 Open Geospatial Consortium, Inc.
- *    All Rights Reserved. http://www.opengeospatial.org/ogc/legal
+ *    This file is hereby placed into the Public Domain.
+ *    This means anyone is free to do whatever they wish with this file.
  *
- *    Permission to use, copy, and modify this software and its documentation, with
- *    or without modification, for any purpose and without fee or royalty is hereby
- *    granted, provided that you include the following on ALL copies of the software
- *    and documentation or portions thereof, including modifications, that you make:
+ *    The Proj.4 wrappers are provided as code examples, in the hope to facilitate
+ *    GeoAPI implementations backed by other libraries. Implementors can take this
+ *    source code and use it for any purpose, commercial or non-commercial, copyrighted
+ *    or open-source, with no legal obligation to acknowledge the borrowing/copying
+ *    in any way.
  *
- *    1. The full text of this NOTICE in a location viewable to users of the
- *       redistributed or derivative work.
- *    2. Notice of any changes or modifications to the OGC files, including the
- *       date changes were made.
- *
- *    THIS SOFTWARE AND DOCUMENTATION IS PROVIDED "AS IS," AND COPYRIGHT HOLDERS MAKE
- *    NO REPRESENTATIONS OR WARRANTIES, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
- *    TO, WARRANTIES OF MERCHANTABILITY OR FITNESS FOR ANY PARTICULAR PURPOSE OR THAT
- *    THE USE OF THE SOFTWARE OR DOCUMENTATION WILL NOT INFRINGE ANY THIRD PARTY
- *    PATENTS, COPYRIGHTS, TRADEMARKS OR OTHER RIGHTS.
- *
- *    COPYRIGHT HOLDERS WILL NOT BE LIABLE FOR ANY DIRECT, INDIRECT, SPECIAL OR
- *    CONSEQUENTIAL DAMAGES ARISING OUT OF ANY USE OF THE SOFTWARE OR DOCUMENTATION.
- *
- *    The name and trademarks of copyright holders may NOT be used in advertising or
- *    publicity pertaining to the software without specific, written prior permission.
- *    Title to copyright in this software and any associated documentation will at all
- *    times remain with copyright holders.
- *
- *
- *    Dual licensing:
  *    A copy of this file is given to the Proj.4 project under their own Open Source license.
  *    Because the "org.proj4" namespace is the property of the Proj.4 project, change to this
  *    file shall be made in collaboration with the Proj.4 project.
