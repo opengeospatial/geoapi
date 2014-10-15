9ea3f40a853b011303389a3a27cbe86b38c70439
desruisseaux <desruisseaux@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2014-10-15 14:09:37 +0000

    Set the version number to 3.1-M05.

--- tags/3.1-M05/tools/src/main/shell/setenv.sh
+++ tags/3.1-M05/tools/src/main/shell/setenv.sh
@@ -3,7 +3,7 @@
 # This provides a single point where to update dependencies.
 # --------------------------------------------------------------
 
-export GEOAPI_VERSION=3.1-SNAPSHOT
+export GEOAPI_VERSION=3.1-M05
 export JSR275_VERSION=0.9.3
 export JUNIT_VERSION=4.11
 export HAMCREST_VERSION=1.3
