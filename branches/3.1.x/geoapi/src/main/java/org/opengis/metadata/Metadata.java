053bf25ebb44ac3336c8f384e57093be905a7189
desruisseaux <desruisseaux@e2b113a6-e024-4c31-8d7a-f4bb93cd2592>
2014-12-01 03:17:16 +0000

    Moved examples before warning box.

--- branches/3.1.x/geoapi/src/main/java/org/opengis/metadata/Metadata.java
+++ branches/3.1.x/geoapi/src/main/java/org/opengis/metadata/Metadata.java
@@ -154,13 +154,6 @@ public interface Metadata {
      * The character coding standard used for the metadata set.
      * Instances can be obtained by a call to {@link Charset#forName(String)}.
      *
-     * <div class="warning"><b>Upcoming API change — JDK integration</b><br>
-     * As of ISO 19115:2014, {@code CharacterSet} is replaced by a reference to the
-     * <a href="http://www.iana.org/assignments/character-sets">IANA Character Set register</a>,
-     * which is represented in Java by {@link java.nio.charset.Charset}.
-     * This change may be applied in GeoAPI 4.0.
-     * </div>
-     *
      * <div class="note"><b>Examples:</b>
      * {@code UCS-2}, {@code UCS-4}, {@code UTF-7}, {@code UTF-8}, {@code UTF-16},
      * {@code ISO-8859-1} (a.k.a. {@code ISO-LATIN-1}), {@code ISO-8859-2}, {@code ISO-8859-3}, {@code ISO-8859-4},
@@ -169,6 +162,13 @@ public interface Metadata {
      * {@code ISO-8859-15}, {@code ISO-8859-16},
      * {@code JIS_X0201}, {@code Shift_JIS}, {@code EUC-JP}, {@code US-ASCII}, {@code EBCDIC}, {@code EUC-KR},
      * {@code Big5}, {@code GB2312}.
+     * </div>
+     *
+     * <div class="warning"><b>Upcoming API change — JDK integration</b><br>
+     * As of ISO 19115:2014, {@code CharacterSet} is replaced by a reference to the
+     * <a href="http://www.iana.org/assignments/character-sets">IANA Character Set register</a>,
+     * which is represented in Java by {@link java.nio.charset.Charset}.
+     * This change may be applied in GeoAPI 4.0.
      * </div>
      *
      * @return Character coding standards used for the metadata.
