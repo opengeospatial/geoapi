6a39c83e82d648681096c33bb46d7283370eec13
desruisseaux <martin.desruisseaux@geomatys.fr>
2016-08-09 15:45:25 +0200

    Specify a FeatureOperationException to be thrown if Operation.apply(...) fail.

---
+++ geoapi/src/main/java/org/opengis/feature/FeatureOperationException.java
@@ -0,0 +1,87 @@
+/*
+ *    GeoAPI - Java interfaces for OGC/ISO standards
+ *    http://www.geoapi.org
+ *
+ *    Copyright (C) 2016 Open Geospatial Consortium, Inc.
+ *    All Rights Reserved. http://www.opengeospatial.org/ogc/legal
+ *
+ *    Permission to use, copy, and modify this software and its documentation, with
+ *    or without modification, for any purpose and without fee or royalty is hereby
+ *    granted, provided that you include the following on ALL copies of the software
+ *    and documentation or portions thereof, including modifications, that you make:
+ *
+ *    1. The full text of this NOTICE in a location viewable to users of the
+ *       redistributed or derivative work.
+ *    2. Notice of any changes or modifications to the OGC files, including the
+ *       date changes were made.
+ *
+ *    THIS SOFTWARE AND DOCUMENTATION IS PROVIDED "AS IS," AND COPYRIGHT HOLDERS MAKE
+ *    NO REPRESENTATIONS OR WARRANTIES, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ *    TO, WARRANTIES OF MERCHANTABILITY OR FITNESS FOR ANY PARTICULAR PURPOSE OR THAT
+ *    THE USE OF THE SOFTWARE OR DOCUMENTATION WILL NOT INFRINGE ANY THIRD PARTY
+ *    PATENTS, COPYRIGHTS, TRADEMARKS OR OTHER RIGHTS.
+ *
+ *    COPYRIGHT HOLDERS WILL NOT BE LIABLE FOR ANY DIRECT, INDIRECT, SPECIAL OR
+ *    CONSEQUENTIAL DAMAGES ARISING OUT OF ANY USE OF THE SOFTWARE OR DOCUMENTATION.
+ *
+ *    The name and trademarks of copyright holders may NOT be used in advertising or
+ *    publicity pertaining to the software without specific, written prior permission.
+ *    Title to copyright in this software and any associated documentation will at all
+ *    times remain with copyright holders.
+ */
+package org.opengis.feature;
+
+import org.opengis.parameter.ParameterValueGroup;
+
+
+/**
+ * Thrown when {@link Operation#apply Operation.apply(…)} is invoked but the operation can not complete.
+ * The operation can fail for a variety of reasons including but not limited to:
+ *
+ * <ul>
+ *   <li>I/O or SQL error while fetching data,</li>
+ *   <li>failure to apply a map projection on a geometric property.</li>
+ * </ul>
+ *
+ * The failure may be caused by illegal arguments, but not necessarily.
+ *
+ * @author  Martin Desruisseaux (Geomatys)
+ * @version 3.1
+ * @since   3.1
+ *
+ * @see Operation#apply(Feature, ParameterValueGroup)
+ */
+public class FeatureOperationException extends RuntimeException {
+    /**
+     * Serial number for inter-operability with different versions.
+     */
+    private static final long serialVersionUID = -2721288981021742180L;
+
+    /**
+     * Creates an exception with no message.
+     */
+    public FeatureOperationException() {
+        super();
+    }
+
+    /**
+     * Creates an exception with the specified message.
+     *
+     * @param  message The detail message. The detail message is saved for
+     *         later retrieval by the {@link #getMessage()} method.
+     */
+    public FeatureOperationException(final String message) {
+        super(message);
+    }
+
+    /**
+     * Creates an exception with the specified message and cause.
+     *
+     * @param  message The detail message. The detail message is saved for
+     *         later retrieval by the {@link #getMessage()} method.
+     * @param  cause The cause.
+     */
+    public FeatureOperationException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+}
