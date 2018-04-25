b58e0eef104ca19642bf1fb99ba11c12e53f6bf5
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2018-04-25 17:04:27 +0200

    Replace <div class="section"> by HTML5 <section>.

--- geoapi-conformance/src/main/java/org/opengis/test/TestSuite.java
+++ geoapi-conformance/src/main/java/org/opengis/test/TestSuite.java
@@ -73,7 +73,8 @@ import org.opengis.referencing.operation.MathTransform;
  * explicitely which factories to use} or {@linkplain #addTestListener being notified of test results}.
  * Those methods take effect even if the {@link TestCase} are run outside of a {@code TestSuite} context.
  *
- * <div class="section">How implementations are discovered</div>
+ * <section class="note">
+ * <h1>How implementations are discovered</h1>
  * All tests use {@link Factory} instances that are specific to the implementation being tested.
  * By default {@code TestSuite} fetches the factory implementations with {@link ServiceLoader},
  * which will scan every <code>META-INF/services/org.opengis.<var>TheFactory</var></code> files
@@ -119,6 +120,7 @@ import org.opengis.referencing.operation.MathTransform;
  * directory if the implementation details shall be honored (otherwise the tests will be run,
  * but the implementation details will be ignored).
  * </div>
+ * </section>
  *
  * @see ImplementationDetails
  * @see TestCase
