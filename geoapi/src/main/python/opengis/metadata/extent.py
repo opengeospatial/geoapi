262a8f71c868138e1f226001ebcbc8eff1fbca54
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2019-01-01 19:31:24 +0100

    Update copyright year. No other change.

--- geoapi/src/main/python/opengis/metadata/extent.py
+++ geoapi/src/main/python/opengis/metadata/extent.py
@@ -2,7 +2,7 @@
 #    GeoAPI - Programming interfaces for OGC/ISO standards
 #    http://www.geoapi.org
 #
-#    Copyright (C) 2018 Open Geospatial Consortium, Inc.
+#    Copyright (C) 2018-2019 Open Geospatial Consortium, Inc.
 #    All Rights Reserved. http://www.opengeospatial.org/ogc/legal
 #
 
