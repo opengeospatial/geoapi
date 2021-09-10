4f44ec5b9c7d2c8e08a2e8a183b4e7b3ad1c245b
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2021-09-10 14:45:31 +0200

    Add a short sentence for 3 pending packages.

---
+++ geoapi/src/pending/java/org/opengis/geometry/primitive/package-info.java
@@ -0,0 +1,40 @@
+/*
+ *    GeoAPI - Java interfaces for OGC/ISO standards
+ *    http://www.geoapi.org
+ *
+ *    Copyright (C) 2004-2021 Open Geospatial Consortium, Inc.
+ *    All Rights Reserved. http://www.opengeospatial.org/ogc/legal
+ *
+ *    Permission to use, copy, and modify this software and its documentation, with
+ *    or without modification, for any purpose and without fee or royalty is hereby
+ *    granted, provided that you include the following on ALL copies of the software
+ *    and documentation or portions thereof, including modifications, that you make:
+ *
+ *    1. The full text of this NOTICE in a location viewable to users of the
+ *       redistributed or derivative work.
+ *    2. Notice of any changes or modifications to the OGC files, including the
+ *       date changes were made.
+ *
+ *    THIS SOFTWARE AND DOCUMENTATION IS PROVIDED "AS IS," AND COPYRIGHT HOLDERS MAKE
+ *    NO REPRESENTATIONS OR WARRANTIES, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ *    TO, WARRANTIES OF MERCHANTABILITY OR FITNESS FOR ANY PARTICULAR PURPOSE OR THAT
+ *    THE USE OF THE SOFTWARE OR DOCUMENTATION WILL NOT INFRINGE ANY THIRD PARTY
+ *    PATENTS, COPYRIGHTS, TRADEMARKS OR OTHER RIGHTS.
+ *
+ *    COPYRIGHT HOLDERS WILL NOT BE LIABLE FOR ANY DIRECT, INDIRECT, SPECIAL OR
+ *    CONSEQUENTIAL DAMAGES ARISING OUT OF ANY USE OF THE SOFTWARE OR DOCUMENTATION.
+ *
+ *    The name and trademarks of copyright holders may NOT be used in advertising or
+ *    publicity pertaining to the software without specific, written prior permission.
+ *    Title to copyright in this software and any associated documentation will at all
+ *    times remain with copyright holders.
+ */
+
+/**
+ * Set of geometric objects that are not decomposed further into other primitives.
+ * The content of this package will be developed in a future GeoAPI release.
+ *
+ * @version 3.1
+ * @since   1.0
+ */
+package org.opengis.geometry.primitive;
