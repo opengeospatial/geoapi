cbc17e535e7aa2577537a0f72d9460706e51bd29
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2023-02-14 18:35:38 +0100

    Edit the files content for Metanorma: - Convert Asciidoc syntax to Metanorma. - Convert MathML formula to AsciiMath. - Convert style sheet customization. - Warnings are not yet fixed.
    Modify Metanorma build for writing in a separated target directory.
    A Unix shell script is added with the needed Metanorma-cli options.
    The intent is to avoid polluting the source directory.
    
    https://github.com/opengeospatial/geoapi/issues/82

---
+++ src/main/metanorma/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Builds GeoAPI specification with Metanorma in a Linux environment.
+# This script assumes that the `metanorma` command-line is available
+# on the path. See https://www.metanorma.org/install/linux/
+#
+# The main purpose of this script is to write the output in the target
+# directory for avoiding to pollute the source directory.
+#
+
+# Stop the script on first error.
+set -o errexit
+
+SOURCE_DIR="`dirname $0`"
+SOURCE_DIR="`realpath $SOURCE_DIR`"
+TARGET_DIR="`realpath $SOURCE_DIR/../../../target`"
+mkdir --parents --verbose $TARGET_DIR
+
+metanorma --output-dir "$TARGET_DIR" --agree-to-terms --type ogc --extensions html document.adoc
+
+# Following directory is empty after a successful build.
+rmdir _files
